857958647a6efb7f0c452d85d372c45cc6aa0f0e
Chetan Surpur <chetan51@gmail.com>
2014-12-18 16:26:42 -0800

    Printing more stats

--- sensorimotor/experiments/capacity/experiment.py
+++ sensorimotor/experiments/capacity/experiment.py
@@ -147,6 +147,10 @@ def run(numWorlds, numElements, outputDir, params=DEFAULTS):
                       showProgressInterval=SHOW_PROGRESS_INTERVAL)
     print
 
+    print MonitorMixinBase.mmPrettyPrintMetrics(
+      runner.tp.mmGetDefaultMetrics() + runner.tm.mmGetDefaultMetrics())
+    print
+
     print "Training temporal pooler..."
     sequences = runner.generateSequences(completeSequenceLength * 1,
                                          exhaustiveAgents,
