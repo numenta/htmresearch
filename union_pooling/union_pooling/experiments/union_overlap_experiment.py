218432af87e7def23a0ca4813e4a9311935e9ad6
Ryan McCall <mccall.ryan@gmail.com>
2015-04-27 17:18:31 -0700

    Monday Funday: parameterz for Union Pooler and exp 1

---
+++ union_pooling/union_pooling/experiments/union_overlap_experiment.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python
+# ----------------------------------------------------------------------
+# Numenta Platform for Intelligent Computing (NuPIC)
+# Copyright (C) 2015, Numenta, Inc.  Unless you have an agreement
+# with Numenta, Inc., for a separate license for this software code, the
+# following terms and conditions apply:
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# http://numenta.org/licenses/
+# ----------------------------------------------------------------------
+import time
+
+from pylab import rcParams
+
+from nupic.data.generators.pattern_machine import PatternMachine
+from nupic.data.generators.sequence_machine import SequenceMachine
+from nupic.research.monitor_mixin.monitor_mixin_base import MonitorMixinBase
+
+from union_pooling.experiments.union_pooler_experiment import (
+    UnionPoolerExperiment)
+
+"""
+Experiment 1
+Union Pooling with and without Temporal Memory training
+Compute overlap between Union SDR representations in two conditions over time
+"""
+
+
+
+_SHOW_PROGRESS_INTERVAL = 100
+_VERBOSITY = 0
+PLOT_RESET_SHADING = 0.2
+PLOT_HEIGHT = 6
+PLOT_WIDTH = 9
+
+
+
+def runTestPhase(experiment, consoleVerbosity):
+  # TODO
+  pass
+
+
+
+def plotNetworkState(experiment, plotVerbosity, trainingPasses, phase=""):
+  print
+  print MonitorMixinBase.mmPrettyPrintMetrics(
+    experiment.tm.mmGetDefaultMetrics())
+  print
+  if plotVerbosity >= 1:
+    rcParams['figure.figsize'] = PLOT_WIDTH, PLOT_HEIGHT
+    title = "training passes: {0}, phase: {1}".format(trainingPasses, phase)
+    experiment.up.mmGetPlotConnectionsPerColumn(title=title)
+    if plotVerbosity >= 2:
+      experiment.tm.mmGetCellActivityPlot(title=title,
+                                          activityType="activeCells",
+                                          showReset=True,
+                                          resetShading=PLOT_RESET_SHADING)
+      experiment.tm.mmGetCellActivityPlot(title=title,
+                                          activityType="predictedActiveCells",
+                                          showReset=True,
+                                          resetShading=PLOT_RESET_SHADING)
+      experiment.up.mmGetCellActivityPlot(title=title, showReset=True,
+                                          resetShading=PLOT_RESET_SHADING)
+
+
+
+def run(trainingPasses, numberOfSequences, sequenceLength,
+        patternDimensionality, patternCardinality, patternAlphabetSize,
+        consoleVerbosity=0, plotVerbosity=0):
+  """
+  Runs the union overlap experiment.
+
+  :param isTrainTemporalMemory: If true the temporal memory will be trained
+  :param numberOfSequences: Number of unique sequences shown to network
+  :param sequenceLength: Length of sequences shown to network
+  :param patternDimensionality: Dimensionality of sequence patterns
+  :param patternCardinality: Cardinality (ON / true bits) of sequence patterns
+  :param patternAlphabetSize: Number of unique patterns from which sequences
+  are built
+  :param consoleVerbosity: Console output verbosity
+  :param plotVerbosity: Plotting verbosity
+  """
+  start = time.time()
+
+  # Generate a sequence list and an associated labeled list (both containing a
+  # set of sequences separated by None)
+  print "Generating sequences..."
+  patternMachine = PatternMachine(patternDimensionality, patternCardinality,
+                                  patternAlphabetSize)
+  sequenceMachine = SequenceMachine(patternMachine)
+
+  numbers = sequenceMachine.generateNumbers(numberOfSequences, sequenceLength)
+  generatedSequences = sequenceMachine.generateFromNumbers(numbers)
+  sequenceLabels = [str(numbers[i + i*sequenceLength: i + (i+1)*sequenceLength])
+                    for i in xrange(numberOfSequences)]
+  labeledSequences = []
+  for label in sequenceLabels:
+    for _ in xrange(sequenceLength):
+      labeledSequences.append(label)
+    labeledSequences.append(None)
+
+  # Set up the Temporal Memory and Union Pooler network
+  print "Creating network..."
+  tmParamOverrides = {}
+  upParamOverrides = {}
+  experiment = UnionPoolerExperiment(tmParamOverrides, upParamOverrides)
+
+  # Train only the Temporal Memory on the generated sequences
+  print "Training Temporal Memory..."
+  for _ in xrange(trainingPasses):
+    experiment.runNetworkOnSequence(generatedSequences,
+                                    labeledSequences,
+                                    tmLearn=True,
+                                    upLearn=None,
+                                    verbosity=_VERBOSITY,
+                                    progressInterval=_SHOW_PROGRESS_INTERVAL)
+  plotNetworkState(experiment, plotVerbosity, trainingPasses,
+                   phase="Training")
+
+  print "Running test phase..."
+  runTestPhase(experiment, consoleVerbosity)
+  plotNetworkState(experiment, plotVerbosity, trainingPasses,
+                   phase="Testing")
+
+  elapsed = int(time.time() - start)
+  print "Total time: {0:2} seconds.".format(elapsed)
+
+  # Write results to output file
+  # TODO writeOutput(outputDir, runner, numElems, numWorlds, elapsed)
+  if plotVerbosity >= 1:
+    raw_input("Press any key to exit...")
+
+
+
+if __name__ == "__main__":
+  trainingPasses = 1
+  numberOfSequences = 4
+  sequenceLength = 4
+  patternDimensionality = 1024
+  patternCardinality = 20
+  patternAlphabetSize = 100
+  run(trainingPasses, numberOfSequences, sequenceLength,
+      patternDimensionality, patternCardinality, patternAlphabetSize)
--- union_pooling/union_pooling/union_pooler.py
+++ union_pooling/union_pooling/union_pooler.py
@@ -19,10 +19,6 @@
 # http://numenta.org/licenses/
 # ----------------------------------------------------------------------
 
-"""
-Experimental Union Pooler Python implementation.
-"""
-
 import numpy
 
 from nupic.bindings.math import (SM32 as SparseMatrix,
@@ -30,14 +26,19 @@ from nupic.bindings.math import (SM32 as SparseMatrix,
 
 from nupic.research.spatial_pooler import SpatialPooler
 
-# TODO: Look into this!
+
+
 REAL_DTYPE = numpy.float32
 
 
 
 class UnionPooler(SpatialPooler):
   """
-
+  Experimental Union Pooler Python implementation. The Union Pooler builds a
+  "union SDR" of the most recent sets of active columns. It is driven by
+  active-cell input and, more strongly, by predictive-active cell input. The
+  latter is more likely to produce active columns. Such winning columns will
+  also tend to persist longer in the union SDR.
   """
 
 
@@ -48,7 +49,7 @@ class UnionPooler(SpatialPooler):
                potentialPct=0.9,
                globalInhibition=True,
                localAreaDensity=-1.0,
-               numActiveColumnsPerInhArea=20.0, # Is this even used?
+               numActiveColumnsPerInhArea=20.0,
                stimulusThreshold=2,
                synPermInactiveDec=0.01,
                synPermActiveInc=0.03,
@@ -60,22 +61,31 @@ class UnionPooler(SpatialPooler):
                seed=42,
                spVerbosity=0,
                wrapAround=True,
-               regularColumnActivationPeriod=100,
-               miniBurstColumnActivationPeriod=500):
+               activeOverlapWeight = 1.0,
+               predictedActiveOverlapWeight = 1.0,
+               activePoolingPeriod=100,
+               predictedActivePoolingPeriod=1000):
     """
-    Please see spatial_pooler.py in NuPIC super class parameter descriptions.
+    Please see spatial_pooler.py in NuPIC for super class parameter
+    descriptions.
 
     Class-specific parameters:
     -------------------------------------
 
-    @param regularColumnActivationPeriod: The maximum number of timesteps that a
-      column activated by only (unpredicted) active cell input will remain
-      active in the Union SDR in the absence of any further active cell input.
+    @param activeOverlapWeight: A multiplicative weight applied to
+    the overlap between connected synapses and active-cell input
 
-    @param miniBurstColumnActivationPeriod: The maximum number of timesteps that
-      a column activated by predicted active cell input will remain
-      active in the Union SDR in the absence of any further predicted
-      active cell input.
+    @param predictedActiveOverlapWeight: A multiplicative weight applied to
+    the overlap between connected synapses and predicted-active-cell input
+
+    @param activePoolingPeriod: The maximum number of timesteps that a
+      column activated by only (unpredicted) active-cell input will remain
+      active in the Union SDR in the absence of any further active-cell input.
+
+    @param predictedActivePoolingPeriod: The maximum number of timesteps that
+      a column activated by predicted-active-cell input will remain
+      active in the Union SDR in the absence of any further
+      predicted-active-cell input.
     """
 
     super(UnionPooler, self).__init__(inputDimensions,
@@ -97,8 +107,10 @@ class UnionPooler(SpatialPooler):
                                       spVerbosity,
                                       wrapAround)
 
-    self._regularColumnActivationPeriod = regularColumnActivationPeriod
-    self._miniBurstColumnActivationPeriod = miniBurstColumnActivationPeriod
+    self._activeOverlapWeight = activeOverlapWeight
+    self._predictedActiveOverlapWeight = predictedActiveOverlapWeight
+    self._activePoolingPeriod = activePoolingPeriod
+    self._predictedActivePoolingPeriod = predictedActivePoolingPeriod
     self._poolingActivation = numpy.zeros(self._numColumns, dtype="int32")
     self._unionSDR = []
 
@@ -120,32 +132,33 @@ class UnionPooler(SpatialPooler):
     self._boostFactors = numpy.ones(self._numColumns, dtype=REAL_DTYPE)
 
 
-  def compute(self, activeCellInput, predictedActiveCellInput, burstingColumns,
+  def compute(self, activeInput, predictedActiveInput, burstingColumns,
               learn):
     """
     Computes one cycle of the Union Pooler algorithm.
     """
-    assert (numpy.size(activeCellInput) == self._numInputs)
-    assert (numpy.size(predictedActiveCellInput) == self._numInputs)
+    assert (numpy.size(activeInput) == self._numInputs)
+    assert (numpy.size(predictedActiveInput) == self._numInputs)
     self._updateBookeepingVars(learn)
 
-    overlapActiveCells = self._calculateOverlap(activeCellInput)
-    overlapPredActiveCells = self._calculateOverlap(predictedActiveCellInput)
-    combinedOverlap = overlapActiveCells + overlapPredActiveCells
+    activeOverlap = self._calculateOverlap(activeInput)
+    predictedActiveOverlap = self._calculateOverlap(predictedActiveInput)
+    totalOverlap = (activeOverlap * self._activeOverlapWeight  +
+                    predictedActiveOverlap * self._predictedActiveOverlapWeight)
 
     # Apply boosting when learning is on
     if learn:
-      boostedOverlaps = self._boostFactors * combinedOverlap
+      boostedOverlaps = self._boostFactors * totalOverlap
     else:
-      boostedOverlaps = combinedOverlap
+      boostedOverlaps = totalOverlap
 
     # Apply inhibition to determine the winning columns
     activeColumns = self._inhibitColumns(boostedOverlaps)
 
     if learn:
-      # TODO Study TP _adaptSynapses to guide this one
+      # TODO Follow TP' _adaptSynapses to guide this one
       # self._adaptSynapses(activeCellInput, activeColumns)
-      self._updateDutyCycles(combinedOverlap, activeColumns)
+      self._updateDutyCycles(totalOverlap, activeColumns)
       self._bumpUpWeakColumns()
       self._updateBoostFactors()
       if self._isUpdateRound():
@@ -158,19 +171,18 @@ class UnionPooler(SpatialPooler):
     self._poolingActivation[self._unionSDR] -= 1
 
     # Set activation of cells receiving input from active cells
-    # TODO Debug and understand this calculation
-    stuff = numpy.where(overlapActiveCells[activeColumns] > 0)[0]
-    activeColsFromActiveCells = activeColumns[stuff]
+    columnIndices = numpy.where(activeOverlap[activeColumns] > 0)[0]
+    activeColsFromActiveCells = activeColumns[columnIndices]
     self._poolingActivation[
-      activeColsFromActiveCells] = self._regularColumnActivationPeriod
+      activeColsFromActiveCells] = self._activePoolingPeriod
 
     # Reset activation of cells receiving predicted input. This ordering assumes
-    # the activation period due to predicted active cells will be
-    # greater than that of standard input
-    stuff = numpy.where(overlapPredActiveCells[activeColumns] > 0)[0]
-    activeColsFromPredActiveCells = activeColumns[stuff]
+    # the activation period due to predicted active cells will be greater than
+    # that of standard input
+    columnIndices = numpy.where(predictedActiveOverlap[activeColumns] > 0)[0]
+    activeColsFromPredActiveCells = activeColumns[columnIndices]
     self._poolingActivation[
-      activeColsFromPredActiveCells] = self._miniBurstColumnActivationPeriod
+      activeColsFromPredActiveCells] = self._predictedActivePoolingPeriod
 
     self._unionSDR = self._poolingActivation.nonzero()[0]
     return self._unionSDR
