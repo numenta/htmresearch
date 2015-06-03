e4ca1780b8cc5f2b22a646be1785b32384d1df6c
Chetan Surpur <chetan51@gmail.com>
2015-06-03 14:01:43 -0700

    Add copyright header

--- vehicle-control/agent/run.py
+++ vehicle-control/agent/run.py
@@ -1,3 +1,25 @@
+#!/usr/bin/env python
+# ----------------------------------------------------------------------
+# Numenta Platform for Intelligent Computing (NuPIC)
+# Copyright (C) 2015, Numenta, Inc.  Unless you have an agreement
+# with Numenta, Inc., for a separate license for this software code, the
+# following terms and conditions apply:
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# http://numenta.org/licenses/
+# ----------------------------------------------------------------------
+
 import numpy
 
 from unity_client.fetcher import Fetcher
@@ -108,10 +130,11 @@ class Plotter(object):
 
 
 
-# complete uniform
-# positions = [i*20 for i in range(36)]
+if __name__ == "__main__":
+  # complete uniform
+  # positions = [i*20 for i in range(36)]
 
-# forward uniform
-positions = [i*10 for i in range(18, 54)]
+  # forward uniform
+  positions = [i*10 for i in range(18, 54)]
 
-run(positions)
+  run(positions)
