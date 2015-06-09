4a1fcfbc1f20f210b5f5557fde434e231a96bd01
Chetan Surpur <chetan51@gmail.com>
2015-06-09 14:51:26 -0700

    Formatting

--- vehicle-control/simulation/Assets/Scripts/SweepSensor.cs
+++ vehicle-control/simulation/Assets/Scripts/SweepSensor.cs
@@ -26,7 +26,8 @@ public class SweepSensor : MonoBehaviour {
 		Vector3 position;
 
 		for (int i = 0; i < numRays; i++) {
-			if (Physics.Raycast(transform.position, direction, out hit) && hit.collider.gameObject.tag != "Boundary") {
+			if (Physics.Raycast(transform.position, direction, out hit) &&
+			    hit.collider.gameObject.tag != "Boundary") {
 				hits[i] = Mathf.Lerp(1, 0, hit.distance / focalLength);
 
 				position = transform.position + direction;
