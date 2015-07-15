2d1f8d84e4ac76f5a84c5fa4b3fbe92bc09b1ed9
Tom <tomssilver@gmail.com>
2015-07-15 16:38:54 -0700

    addressing CR feedback in preparation for demo

--- encoder_quality/rdse_check_demo.py
+++ encoder_quality/rdse_check_demo.py
@@ -19,6 +19,20 @@
 # http://numenta.org/licenses/
 # ----------------------------------------------------------------------
 
+"""
+This script demonstrates how encoder_check.encoderCheck is used.
+
+The example shows that a RandomDistributedScalarEncoder with higher
+resolution will more tightly preserve the distance metric of the scalar
+input space.
+
+For three scalar values x, y, z, and their encodings Sx, Sy, and Sz, if
+the overlap of Sx and Sy is greater than the overlap of Sx and Sz, we would
+hope that the distance between x and y is less than the distance between x and
+z. This is the logic that the encoderCheck employs. If it finds values that
+violate this property, it reports it with a warning.
+"""
+
 import encoder_check
 import numpy as np
 
@@ -35,10 +49,10 @@ if __name__ == "__main__":
   Nsamples = 1000
 
   encoder1 = RandomDistributedScalarEncoder(name="encoder", resolution=1.0,
-                                             w=23, n=500, offset=0.0)
+                                            w=23, n=500, offset=0.0)
 
   encoder2 = RandomDistributedScalarEncoder(name="encoder", resolution=10.0,
-                                           w=23, n=500, offset=0.0)
+                                            w=23, n=500, offset=0.0)
 
 
   distance_function = lambda x,y : abs(x-y)
@@ -47,13 +61,13 @@ if __name__ == "__main__":
   input_pairs_source = encoder_check.InputTripleCreator(sample_generator)
 
   err1 = encoder_check.encoderCheck(encoder1, distance_function,
-  	                               input_pairs_source)
+                                    input_pairs_source)
 
   err2 = encoder_check.encoderCheck(encoder2, distance_function,
-                                   input_pairs_source)
+                                    input_pairs_source)
 
   print
-  print "Average error for encoder w/ resolution 1.0: ",
+  print "Warning rate for encoder w/ resolution 1.0: ",
   print err1
-  print "Average error for encoder w/ resolution 10.0: ",
+  print "Warning rate for encoder w/ resolution 10.0: ",
   print err2
