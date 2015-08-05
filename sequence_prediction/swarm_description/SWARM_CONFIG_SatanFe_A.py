a48e8c57f162706bdaa92465a4c6fb00a31c5622
Yuwei <ywcui1990@gmail.com>
2015-08-05 09:54:29 -0700

    add swarm_description files

---
+++ sequence_prediction/swarm_description/SWARM_CONFIG_SatanFe_A.py
@@ -0,0 +1,52 @@
+# ----------------------------------------------------------------------
+# Numenta Platform for Intelligent Computing (NuPIC)
+# Copyright (C) 2015, Numenta, Inc.  Unless you have an agreement
+# with Numenta, Inc., for a separate license for this software code, the
+# following terms and conditions apply:
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# http://numenta.org/licenses/
+# ----------------------------------------------------------------------
+
+
+SWARM_CONFIG = {
+  "includedFields": [
+    {
+      "fieldName": "data",
+      "fieldType": "float",
+      "maxValue": 256,
+      "minValue": 0,
+    }
+  ],
+  "streamDef": {
+    "info": "laser_data",
+    "version": 1,
+    "streams": [
+      {
+        "info": "SantaFe_A.csv",
+        "source": "file://data/SantaFe_A.csv",
+        "columns": [
+          "*"
+        ]
+      }
+    ]
+  },
+  "inferenceType": "TemporalMultiStep",
+  "inferenceArgs": {
+    "predictionSteps": [1],
+    "predictedField": "data"
+  },
+  "metricWindow": 1000,
+  "swarmSize": "large"
+}
