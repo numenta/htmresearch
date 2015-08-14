4de983e2aa4e1678e4dcb3b286b5c342a875ffd1
Chetan Surpur <chetan51@gmail.com>
2015-08-13 19:26:47 -0700

    Switch resets between sequences to random elements

--- sequence_prediction/sequence_generator/predict.py
+++ sequence_prediction/sequence_generator/predict.py
@@ -37,8 +37,10 @@ from sequence_generator import SequenceGenerator
 
 MIN_ORDER = 6
 MAX_ORDER = 7
-NUM_PREDICTIONS = 2
+NUM_PREDICTIONS = 1
+NUM_RANDOM = 1
 
+RANDOM_RESERVOIR = 1000
 NUM_SYMBOLS = SequenceGenerator.numSymbols(MAX_ORDER, NUM_PREDICTIONS)
 
 MODEL_PARAMS = {
@@ -54,7 +56,7 @@ MODEL_PARAMS = {
           "fieldname": u"element",
           "name": u"element",
           "type": "SDRCategoryEncoder",
-          "categoryList": range(NUM_SYMBOLS),
+          "categoryList": range(NUM_SYMBOLS + RANDOM_RESERVOIR),
           "n": 2048,
           "w": 41
         }
@@ -165,38 +167,38 @@ def generateSequences():
   # ]
   # random.seed(100) # 100 fails, 300 works (results depend on order of training)
 
-  # # Hardcoded set of sequences
-  # sequences = [
-  #   [6, 8, 7, 4, 2, 3, 0],
-  #   [6, 3, 4, 2, 7, 8, 5],
-  #   [1, 8, 7, 4, 2, 3, 5],
-  #   [1, 3, 4, 2, 7, 8, 0],
-  #   [1, 9, 7, 8, 5, 3, 4, 0],
-  #   [1, 4, 3, 5, 8, 7, 9, 6],
-  #   [2, 9, 7, 8, 5, 3, 4, 6],
-  #   [2, 4, 3, 5, 8, 7, 9, 0]
-  # ]
-
-  # Hardcoded set of sequences with multiple predictions (2)
-  # Make sure to set NUM_PREDICTIONS = 2 above
+  # Hardcoded set of sequences
   sequences = [
-    [4, 8, 3, 10, 9, 6, 1],
-    [4, 6, 9, 10, 3, 8, 7],
-    [4, 8, 3, 10, 9, 6, 2],
-    [4, 6, 9, 10, 3, 8, 0],
-    [5, 8, 3, 10, 9, 6, 0],
-    [5, 6, 9, 10, 3, 8, 2],
-    [5, 8, 3, 10, 9, 6, 7],
-    [5, 6, 9, 10, 3, 8, 1],
-    [4, 3, 8, 6, 1, 10, 11, 9],
-    [4, 11, 10, 1, 6, 8, 3, 7],
-    [4, 3, 8, 6, 1, 10, 11, 2],
-    [4, 11, 10, 1, 6, 8, 3, 0],
-    [5, 3, 8, 6, 1, 10, 11, 0],
-    [5, 11, 10, 1, 6, 8, 3, 2],
-    [5, 3, 8, 6, 1, 10, 11, 7],
-    [5, 11, 10, 1, 6, 8, 3, 9]
+    [6, 8, 7, 4, 2, 3, 0],
+    [6, 3, 4, 2, 7, 8, 5],
+    [1, 8, 7, 4, 2, 3, 5],
+    [1, 3, 4, 2, 7, 8, 0],
+    [1, 9, 7, 8, 5, 3, 4, 0],
+    [1, 4, 3, 5, 8, 7, 9, 6],
+    [2, 9, 7, 8, 5, 3, 4, 6],
+    [2, 4, 3, 5, 8, 7, 9, 0]
   ]
+
+  # # Hardcoded set of sequences with multiple predictions (2)
+  # # Make sure to set NUM_PREDICTIONS = 2 above
+  # sequences = [
+  #   [4, 8, 3, 10, 9, 6, 1],
+  #   [4, 6, 9, 10, 3, 8, 7],
+  #   [4, 8, 3, 10, 9, 6, 2],
+  #   [4, 6, 9, 10, 3, 8, 0],
+  #   [5, 8, 3, 10, 9, 6, 0],
+  #   [5, 6, 9, 10, 3, 8, 2],
+  #   [5, 8, 3, 10, 9, 6, 7],
+  #   [5, 6, 9, 10, 3, 8, 1],
+  #   [4, 3, 8, 6, 1, 10, 11, 9],
+  #   [4, 11, 10, 1, 6, 8, 3, 7],
+  #   [4, 3, 8, 6, 1, 10, 11, 2],
+  #   [4, 11, 10, 1, 6, 8, 3, 0],
+  #   [5, 3, 8, 6, 1, 10, 11, 0],
+  #   [5, 11, 10, 1, 6, 8, 3, 2],
+  #   [5, 3, 8, 6, 1, 10, 11, 7],
+  #   [5, 11, 10, 1, 6, 8, 3, 9]
+  # ]
 
   for sequence in sequences:
     print sequence
@@ -364,4 +366,13 @@ if __name__ == "__main__":
           plotTMStats(numPredictedActiveCells, numPredictedInactiveCells, numUnpredictedActiveColumns)
           pyplot.draw()
 
-    model.resetSequenceStates()
+    # Feed noise
+    sequence = range(NUM_SYMBOLS, NUM_SYMBOLS + RANDOM_RESERVOIR)
+    random.shuffle(sequence)
+    sequence = sequence[0:NUM_RANDOM]
+    print "Random:", sequence
+
+    for element in sequence:
+      model.run({"element": element})
+
+    print
