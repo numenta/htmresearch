a902f502f8ac3f1bb3f2fbc42e5a4d1c03a9c728
Yuwei <ywcui1990@gmail.com>
2015-09-01 15:04:06 -0700

    update swarm descriptioins

--- sequence_prediction/continuous_sequence/swarm_description/SWARM_CONFIG_nyc_taxi.py
+++ sequence_prediction/continuous_sequence/swarm_description/SWARM_CONFIG_nyc_taxi.py
@@ -1,19 +1,19 @@
 # ----------------------------------------------------------------------
 # Numenta Platform for Intelligent Computing (NuPIC)
-# Copyright (C) 2015, Numenta, Inc.  Unless you have an agreement
+# Copyright (C) 2013-2015, Numenta, Inc.  Unless you have an agreement
 # with Numenta, Inc., for a separate license for this software code, the
 # following terms and conditions apply:
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 3 as
+# it under the terms of the GNU Affero Public License version 3 as
 # published by the Free Software Foundation.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the GNU General Public License for more details.
+# See the GNU Affero Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
+# You should have received a copy of the GNU Affero Public License
 # along with this program.  If not, see http://www.gnu.org/licenses.
 #
 # http://numenta.org/licenses/
@@ -22,14 +22,20 @@
 
 SWARM_CONFIG = {
   "includedFields": [
-    {
-      "fieldName": "timestamp",
-      "fieldType": "datetime"
-    },
+    # {
+    #   "fieldName": "timeofday",
+    #   "fieldType": "string",
+    #   "maxValue": 1500,
+    #   "minValue": 0
+    # },
+    # {
+    #   "fieldName": "dayofweek",
+    #   "fieldType": "string"
+    # },
     {
       "fieldName": "passenger_count",
       "fieldType": "int",
-      "maxValue": 32000,
+      "maxValue": 40000,
       "minValue": 0
     }
   ],
@@ -43,7 +49,7 @@ SWARM_CONFIG = {
         "columns": [
           "*"
         ],
-        "last_record": 8000
+        "last_record": 5000
       }
     ],
   },
@@ -51,11 +57,11 @@ SWARM_CONFIG = {
   "inferenceType": "TemporalMultiStep",
   "inferenceArgs": {
     "predictionSteps": [
-      1, 5
+      5
     ],
     "predictedField": "passenger_count"
   },
   "metricWindow": 2000,
   "iterationCount": -1,
-  "swarmSize": "medium"
+  "swarmSize": "large"
 }
