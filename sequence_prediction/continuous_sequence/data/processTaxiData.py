74f85a45ad060820f41cd0a7f0f252367ade1c17
Yuwei <ywcui1990@gmail.com>
2015-09-01 15:03:00 -0700

    update nyc taxi dataset

--- sequence_prediction/continuous_sequence/data/processTaxiData.py
+++ sequence_prediction/continuous_sequence/data/processTaxiData.py
@@ -3,6 +3,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 import os
 
+from pygeocoder import Geocoder
+
 plt.ion()
 
 year = 2015
@@ -18,6 +20,18 @@ for year in [2014, 2015]:
 
     if os.path.isfile(datafileName):
       print " Load Datafile: ", datafileName
+      # df = pd.read_csv(datafileName, header=0, nrows=100,  usecols=[1, 3, 5, 6],
+      #                  names=['pickup_datetime', 'passenger_count', 'pickup_longitude', 'pickup_latitude'])
+      #
+      # postcode = np.zeros(len(df))
+      # for i in xrange(len(df)):
+      #   try:
+      #     results = Geocoder.reverse_geocode(df['pickup_latitude'][i], df['pickup_longitude'][i])
+      #     postcode[i] = results.postal_code
+      #   except:
+      #     pass
+
+
       df = pd.read_csv(datafileName, header=0, usecols=[1, 3], names=['pickup_datetime', 'passenger_count'])
 
       record_num.append(len(df))
@@ -41,7 +55,6 @@ for year in [2014, 2015]:
 print "include time of day and day of week as input field"
 date = ts_all.index
 dayofweek = (date.dayofweek)
-# timeofday = date.dt.time
 timeofday = (date.hour*60 + date.minute)
 passenger_count = np.array(ts_all['Sum'])
 seq = pd.DataFrame(np.transpose(np.array([passenger_count, timeofday, dayofweek])), columns=['passenger_count', 'timeofday', 'dayofweek'], index=ts_all.index)
@@ -54,8 +67,8 @@ import csv
 outputFile = open('nyc_taxi.csv',"w")
 csvWriter = csv.writer(outputFile)
 csvWriter.writerow(['timestamp', 'passenger_count', 'timeofday', 'dayofweek'])
-csvWriter.writerow(['datetime', 'int', 'int', 'int'])
-csvWriter.writerow(['T', ''])
+csvWriter.writerow(['datetime', 'int', 'int', 'string'])
+csvWriter.writerow(['T', '', '', ''])
 for i in range(len(ts_all)):
   csvWriter.writerow([seq.index[i], seq.passenger_count[i], seq.timeofday[i], seq.dayofweek[i]])
 outputFile.close()
