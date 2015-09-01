9d07c95d5c1bb000afe8d5f4bbe0b2ebbc26ecc7
Yuwei <ywcui1990@gmail.com>
2015-09-01 15:02:30 -0700

    update model runners

--- sequence_prediction/continuous_sequence/comparePerformance.py
+++ sequence_prediction/continuous_sequence/comparePerformance.py
@@ -1,19 +1,19 @@
 # ----------------------------------------------------------------------
 # Numenta Platform for Intelligent Computing (NuPIC)
-# Copyright (C) 2015, Numenta, Inc.  Unless you have an agreement
+# Copyright (C) 2013-2015, Numenta, Inc.  Unless you have an agreement
 # with Numenta, Inc., for a separate license for this software code, the
 # following terms and conditions apply:
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 3 as
+# it under the terms of the GNU Affero Public License version 3 as
 # published by the Free Software Foundation.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the GNU General Public License for more details.
+# See the GNU Affero Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
+# You should have received a copy of the GNU Affero Public License
 # along with this program.  If not, see http://www.gnu.org/licenses.
 #
 # http://numenta.org/licenses/
@@ -25,71 +25,98 @@ import matplotlib.pyplot as plt
 from matplotlib import rcParams
 from optparse import OptionParser
 from swarm_runner import SwarmRunner
-
+from nrmse import *
 rcParams.update({'figure.autolayout': True})
 plt.ion()
 plt.close('all')
 
-def loadDataFile(filePath):
 
+def loadTrueDataFile(filePath):
   data = pd.read_csv(filePath, header=0, nrows=1)
-  ncol = len(data.columns.values)
+  colnames = data.columns.values
+  ncol = len(colnames)
+
+  # data = pd.read_csv(filePath, header=0, skiprows=[1,2], names=['step', 'value', 'prediction'])
+
   if ncol == 2:
     data = pd.read_csv(filePath, header=0, skiprows=[1,2], names=['step', 'value'])
   elif ncol == 3:
-    data = pd.read_csv(filePath, header=0, skiprows=[1,2], names=['step', 'value', 'prediction1'])
+    data = pd.read_csv(filePath, header=0, skiprows=[1,2], names=['step', 'value', 'prediction'])
   elif ncol == 4:
-    data = pd.read_csv(filePath, header=0, skiprows=[1,2], names=['step', 'data', 'prediction1','prediction5'])
+    data = pd.read_csv(filePath, header=0, skiprows=[1,2], names=['step', 'value', 'timeofday', 'dayofweek'])
   return data
 
-def NRMSE(data, pred):
-  return np.sqrt(np.nanmean(np.square(pred-data)))/np.sqrt(np.nanmean( np.square(data-np.nanmean(data))))
+
+
+def loadDataFileLSTM(filePath):
+  data = pd.read_csv(filePath, header=0, nrows=1)
+  colnames = data.columns.values
+  ncol = len(colnames)
+  data = pd.read_csv(filePath, header=0, skiprows=[1,2], names=['step', 'value', 'prediction5'])
+
+  return data
+
+
+def loadDataFile(filePath):
+  data = pd.read_csv(filePath, header=0, nrows=1)
+  colnames = data.columns.values
+  ncol = len(colnames)
+
+  # data = pd.read_csv(filePath, header=0, skiprows=[1,2], names=['step', 'value', 'prediction'])
+
+  if ncol == 2:
+    data = pd.read_csv(filePath, header=0, skiprows=[1,2], names=['step', 'value'])
+  elif ncol == 3:
+    data = pd.read_csv(filePath, header=0, skiprows=[1,2], names=['step', 'value', 'prediction'])
+  elif ncol == 4:
+    data = pd.read_csv(filePath, header=0, skiprows=[1,2], names=['step', 'data', 'prediction1', 'prediction5'])
+  return data
 
 
 def plotPerformance(dataSet, nTrain):
-  filePath = './data/' + dataSet + '.csv'
-  print "load test data from ", filePath
-  trueData = loadDataFile(filePath)
-  trueData = trueData['value']
-
-  filePath = './prediction/' + dataSet + '_TM_pred.csv'
-  print "load TM prediction from ", filePath
-  predData_TM = loadDataFile(filePath)
-
-  filePath = './prediction/' + dataSet + '_ARIMA_pred.csv'
-  predData_ARIMA = loadDataFile(filePath)
-
-  print "nTrain: ", nTrain
-  print "nTest: ", len(trueData[nTrain:])
-
-  # trivial shift predictor
-  predData_shift = np.roll(trueData, 1)
-  predData_TM_one_step = np.roll(predData_TM['prediction1'], 1)
-  predData_ARIMA_one_step = np.roll(predData_ARIMA['prediction1'], 1)
-
-  trueData = trueData[nTrain:]
-  predData_TM_one_step = predData_TM_one_step[nTrain:]
-  predData_shift = predData_shift[nTrain:]
-  predData_ARIMA_one_step = predData_ARIMA_one_step[nTrain:]
-
-  NRMSE_TM = NRMSE(trueData, predData_TM_one_step)
-  NRMSE_ARIMA = NRMSE(trueData, predData_ARIMA_one_step)
-  NRMSE_Shift = NRMSE(trueData, predData_shift)
-
-  print "NRMSE: Shift - 1step", NRMSE_Shift
-  print "NRMSE: TM - 1 step", NRMSE_TM
-  print "NRMSE: ARIMA - 1 step", NRMSE_ARIMA
-
-  plt.figure(1)
-  plt.plot(trueData, label='True Data', color='black')
-  plt.plot(predData_shift, label='Trival NRMSE: '+"%0.3f" % NRMSE_Shift)
-  plt.plot(predData_ARIMA_one_step, label='ARIMA NRMSE: '+"%0.3f" % NRMSE_ARIMA)
-  plt.plot(predData_TM_one_step, label='TM, NRMSE: '+"%0.3f" % NRMSE_TM)
-  plt.legend()
-  plt.xlabel('Time')
-  fileName = './result/'+dataSet+"modelPrediction.pdf"
-  print "save example prediction trace to ", fileName
-  plt.savefig(fileName)
+  # filePath = './data/' + dataSet + '.csv'
+  # print "load test data from ", filePath
+  # trueData = loadDataFile(filePath)
+  # trueData = trueData['value']
+  #
+  # filePath = './prediction/' + dataSet + '_TM_pred.csv'
+  # print "load TM prediction from ", filePath
+  # predData_TM = loadDataFile(filePath)
+  #
+  # filePath = './prediction/' + dataSet + '_ARIMA_pred.csv'
+  # predData_ARIMA = loadDataFile(filePath)
+  #
+  # print "nTrain: ", nTrain
+  # print "nTest: ", len(trueData[nTrain:])
+  #
+  # # trivial shift predictor
+  # predData_shift = np.roll(trueData, 1)
+  # predData_TM_one_step = np.roll(predData_TM['prediction1'], 1)
+  # predData_ARIMA_one_step = np.roll(predData_ARIMA['prediction1'], 1)
+  #
+  # trueData = trueData[nTrain:]
+  # predData_TM_one_step = predData_TM_one_step[nTrain:]
+  # predData_shift = predData_shift[nTrain:]
+  # predData_ARIMA_one_step = predData_ARIMA_one_step[nTrain:]
+  #
+  # NRMSE_TM = NRMSE(trueData, predData_TM_one_step)
+  # NRMSE_ARIMA = NRMSE(trueData, predData_ARIMA_one_step)
+  # NRMSE_Shift = NRMSE(trueData, predData_shift)
+  #
+  # print "NRMSE: Shift - 1step", NRMSE_Shift
+  # print "NRMSE: TM - 1 step", NRMSE_TM
+  # print "NRMSE: ARIMA - 1 step", NRMSE_ARIMA
+  #
+  # plt.figure(1)
+  # plt.plot(trueData, label='True Data', color='black')
+  # plt.plot(predData_shift, label='Trival NRMSE: '+"%0.3f" % NRMSE_Shift)
+  # plt.plot(predData_ARIMA_one_step, label='ARIMA NRMSE: '+"%0.3f" % NRMSE_ARIMA)
+  # plt.plot(predData_TM_one_step, label='TM, NRMSE: '+"%0.3f" % NRMSE_TM)
+  # plt.legend()
+  # plt.xlabel('Time')
+  # fileName = './result/'+dataSet+"modelPrediction.pdf"
+  # print "save example prediction trace to ", fileName
+  # plt.savefig(fileName)
 
   # resTM = abs(trueData-predData_TM)
   # res_shift = abs(trueData-predData_shift)
@@ -114,51 +141,88 @@ def plotPerformance(dataSet, nTrain):
 
   filePath = './data/' + dataSet + '.csv'
   print "load test data from ", filePath
-  trueData = loadDataFile(filePath)
+  trueData = loadTrueDataFile(filePath)
   trueData = trueData['value'].astype('float')
 
   filePath = './prediction/' + dataSet + '_TM_pred.csv'
   print "load TM prediction from ", filePath
-  predData_TM = loadDataFile(filePath)
+  predData_TM = loadDataFileLSTM(filePath)
 
   filePath = './prediction/' + dataSet + '_ARIMA_pred.csv'
   predData_ARIMA = loadDataFile(filePath)
 
+  filePath = './prediction/' + dataSet + '_lstm_pred.csv'
+  predData_LSTM = loadDataFileLSTM(filePath)
+
+
+  nTest = len(trueData) - nTrain - 5
   print "nTrain: ", nTrain
   print "nTest: ", len(trueData[nTrain:])
 
   # trivial shift predictor
   predData_shift = np.roll(trueData, 5)
   predData_TM_five_step = np.roll(predData_TM['prediction5'], 5)
-  predData_ARIMA_five_step = np.roll(predData_ARIMA['prediction5'], 5)
+  predData_ARIMA_five_step = predData_shift
+  # predData_ARIMA_five_step = np.roll(predData_ARIMA['prediction5'], 5)
+  predData_LSTM_five_step = np.roll(predData_LSTM['prediction5'], 5)
+
   time_step = predData_TM['step']
 
-  trueData = trueData[nTrain:]
-  predData_TM_five_step = predData_TM_five_step[nTrain:]
-  predData_shift = predData_shift[nTrain:]
-  predData_ARIMA_five_step = predData_ARIMA_five_step[nTrain:]
-  time_step = time_step[nTrain:]
+  nrmse_window = 1000
+  (window_center, nrmse_slide_tm) = NRMSE_sliding(trueData, predData_TM_five_step, nrmse_window)
+  (window_center, nrmse_slide_lstm) = NRMSE_sliding(trueData, predData_LSTM_five_step, nrmse_window)
+  (window_center, nrmse_slide_shift) = NRMSE_sliding(trueData, predData_shift, nrmse_window)
+
+  plt.figure(1)
+  plt.plot(window_center, nrmse_slide_tm, label='TM')
+  plt.plot(window_center, nrmse_slide_lstm, label='LSTM')
+  plt.plot(window_center, nrmse_slide_shift, label='Shift')
+  plt.legend()
+
+  trueData = trueData[nTrain:nTrain+nTest]
+  predData_TM_five_step = predData_TM_five_step[nTrain:nTrain+nTest]
+  predData_shift = predData_shift[nTrain:nTrain+nTest]
+  predData_ARIMA_five_step = predData_ARIMA_five_step[nTrain:nTrain+nTest]
+  predData_LSTM_five_step = predData_LSTM_five_step[nTrain:nTrain+nTest]
+  time_step = time_step[nTrain:nTrain+nTest]
 
   NRMSE_TM = NRMSE(trueData, predData_TM_five_step)
   NRMSE_ARIMA = NRMSE(trueData, predData_ARIMA_five_step)
+  NRMSE_LSTM = NRMSE(trueData, predData_LSTM_five_step)
   NRMSE_Shift = NRMSE(trueData, predData_shift)
+
+
 
   print "NRMSE: Shift - 5 step", NRMSE_Shift
   print "NRMSE: TM - 5 step", NRMSE_TM
   print "NRMSE: ARIMA - 5 step", NRMSE_ARIMA
+  print "NRMSE: LSTM - 5 step", NRMSE_LSTM
 
   time_step = pd.to_datetime(time_step)
   plt.figure(2)
   plt.plot(time_step, trueData, label='True Data', color='black')
-  plt.plot(time_step, predData_shift, label='Trival NRMSE: '+"%0.3f" % NRMSE_Shift)
-  plt.plot(time_step, predData_ARIMA_five_step, label='ARIMA NRMSE: '+"%0.3f" % NRMSE_ARIMA)
+  # plt.plot(time_step, predData_shift, label='Trival NRMSE: '+"%0.3f" % NRMSE_Shift)
+  # plt.plot(time_step, predData_ARIMA_five_step, label='ARIMA NRMSE: '+"%0.3f" % NRMSE_ARIMA)
   plt.plot(time_step, predData_TM_five_step, label='TM, NRMSE: '+"%0.3f" % NRMSE_TM)
+  plt.plot(time_step, predData_LSTM_five_step, label='LSTM, NRMSE: '+"%0.3f" % NRMSE_LSTM)
   plt.legend()
   plt.xlabel('Time')
-  plt.xlim([time_step.values[200], time_step.values[500]])
+  plt.ylabel('Passenger Count')
+  plt.xlim([time_step.values[0], time_step.values[400]])
   fileName = './result/'+dataSet+"modelPrediction.pdf"
   print "save example prediction trace to ", fileName
   plt.savefig(fileName)
+
+  fig, ax = plt.subplots()
+  inds = np.arange(4)
+  ax.bar(inds, [NRMSE_Shift, NRMSE_LSTM, NRMSE_ARIMA, NRMSE_TM], width=0.3)
+  ax.set_xticks(inds+0.3/2)
+  ax.set_xticklabels( ('Shift', 'LSTM', 'ARIMA', 'TM') )
+  ax.set_ylabel('NRMSE')
+
+  # import plotly.plotly as py
+  # plot_url = py.plot_mpl(fig)
+
 
 def _getArgs():
   parser = OptionParser(usage="%prog PARAMS_DIR OUTPUT_DIR [options]"
--- sequence_prediction/continuous_sequence/run_model_timestamp.py
+++ sequence_prediction/continuous_sequence/run_model_timestamp.py
@@ -1,28 +1,24 @@
-#!/usr/bin/env python
-# ----------------------------------------------------------------------
+## ----------------------------------------------------------------------
 # Numenta Platform for Intelligent Computing (NuPIC)
-# Copyright (C) 2013, Numenta, Inc.  Unless you have an agreement
+# Copyright (C) 2013-2015, Numenta, Inc.  Unless you have an agreement
 # with Numenta, Inc., for a separate license for this software code, the
 # following terms and conditions apply:
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 3 as
+# it under the terms of the GNU Affero Public License version 3 as
 # published by the Free Software Foundation.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the GNU General Public License for more details.
+# See the GNU Affero Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
+# You should have received a copy of the GNU Affero Public License
 # along with this program.  If not, see http://www.gnu.org/licenses.
 #
 # http://numenta.org/licenses/
 # ----------------------------------------------------------------------
-"""
-Groups together code used for creating a NuPIC model and dealing with IO.
-(This is a component of the One Hot Gym Prediction Tutorial.)
-"""
+
 import importlib
 import sys
 import csv
@@ -32,9 +28,12 @@ from nupic.data.inference_shifter import InferenceShifter
 from nupic.frameworks.opf.metrics import MetricSpec
 from nupic.frameworks.opf.modelfactory import ModelFactory
 from nupic.frameworks.opf.predictionmetricsmanager import MetricsManager
-
+from nupic.frameworks.opf import metrics
 import nupic_output
 from optparse import OptionParser
+
+import numpy as np
+import matplotlib.pyplot as plt
 
 DESCRIPTION = (
   "Starts a NuPIC model from the model params returned by the swarm\n"
@@ -55,10 +54,10 @@ def getMetricSpecs(predictedField):
   _METRIC_SPECS = (
       MetricSpec(field=predictedField, metric='multiStep',
                  inferenceElement='multiStepBestPredictions',
-                 params={'errorMetric': 'aae', 'window': 1000, 'steps': 1}),
+                 params={'errorMetric': 'negativeLogLikelihood', 'window': 1000, 'steps': 5}),
       MetricSpec(field=predictedField, metric='multiStep',
                  inferenceElement='multiStepBestPredictions',
-                 params={'errorMetric': 'nrmse', 'window': 1000, 'steps': 1}),
+                 params={'errorMetric': 'nrmse', 'window': 1000, 'steps': 5}),
   )
   return _METRIC_SPECS
 
@@ -115,24 +114,28 @@ def runIoThroughNupic(inputData, model, dataSet, plot, savePrediction=True):
     elif dataSet == 'nyc_taxi':
       result = model.run({
         "timestamp": timestamp,
-        "passenger_count": data
+        "passenger_count": float(row[1])
       })
 
     result.metrics = metricsManager.update(result)
 
     if counter % 100 == 0:
       print "Read %i lines..." % counter
-      print ("After %i records, 1-step NRMSE=%f", counter,
-              result.metrics["multiStepBestPredictions:multiStep:"
-                             "errorMetric='nrmse':steps=1:window=1000:"
-                             "field="+predictedField])
+      negLL = result.metrics["multiStepBestPredictions:multiStep:"
+                             "errorMetric='negativeLogLikelihood':steps=5:window=1000:"
+                             "field="+predictedField]
+      nrmse = result.metrics["multiStepBestPredictions:multiStep:"
+                             "errorMetric='nrmse':steps=5:window=1000:"
+                             "field="+predictedField]
+      print "After %i records, 5-step negLL=%f nrmse=%f" % (counter, negLL, nrmse)
  
     if plot:
       result = shifter.shift(result)
 
-    prediction_1step = result.inferences["multiStepBestPredictions"][1]
+    # prediction_1step = result.inferences["multiStepBestPredictions"][1]
     prediction_5step = result.inferences["multiStepBestPredictions"][5]
-    output.write([timestamp], [data], [prediction_1step], [prediction_5step])
+    output.write([timestamp], [data], [prediction_5step])
+    # output.write([timestamp], [data], [prediction_1step], [prediction_5step])
 
   inputFile.close()
   output.close()
@@ -187,4 +190,164 @@ if __name__ == "__main__":
     DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
     predictedField = "passenger_count"
 
-  runModel(dataSet, plot=plot)
+  # runModel(dataSet, plot=plot)
+
+  print "Creating model from %s..." % dataSet
+  model = createModel(getModelParamsFromName(dataSet))
+  model.enableLearning()
+  # model.disableLearning()
+  inputData = "%s/%s.csv" % (DATA_DIR, dataSet.replace(" ", "_"))
+
+
+  sensor = model._getSensorRegion()
+  encoderList = sensor.getSelf().encoder.getEncoderList()
+  if sensor.getSelf().disabledEncoder is not None:
+    classifier_encoder = sensor.getSelf().disabledEncoder.getEncoderList()
+    classifier_encoder = classifier_encoder[0]
+  else:
+    classifier_encoder = None
+
+  # encoder = model._classifierInputEncoder
+  maxBucket = classifier_encoder.n - classifier_encoder.w + 1
+  likelihoodsVecAll = np.zeros((maxBucket, 10000))
+
+  print dataSet
+  inputFile = open(inputData, "rb")
+  csvReader = csv.reader(inputFile)
+  # skip header rows
+  csvReader.next()
+  csvReader.next()
+  csvReader.next()
+
+  shifter = InferenceShifter()
+  output = nupic_output.NuPICFileOutput([dataSet])
+
+  _METRIC_SPECS = getMetricSpecs(predictedField)
+  metric = metrics.getModule(_METRIC_SPECS[0])
+  metricsManager = MetricsManager(_METRIC_SPECS, model.getFieldInfo(),
+                                  model.getInferenceType())
+
+  counter = 0
+  prediction_5step = None
+
+
+  likelihoodsVecAll = np.zeros((maxBucket, 20000))
+
+  time_step = []
+  actual_data = []
+  patternNZ_track = []
+  nPredictionSteps = 5
+  predict_data = np.zeros((nPredictionSteps, 0))
+  predict_data_ML = []
+  negLL_track = []
+
+  activeColumnNum = []
+
+  if plot:
+    import matplotlib.gridspec as gridspec
+    plotCount = 1
+    plotHeight = max(plotCount * 3, 6)
+    fig = plt.figure(figsize=(14, plotHeight))
+    gs = gridspec.GridSpec(plotCount, 1)
+    plt.title(predictedField)
+    plt.ylabel('Data')
+    plt.xlabel('Timed')
+    plt.tight_layout()
+    plt.ion()
+
+  for row in csvReader:
+    timestamp = datetime.datetime.strptime(row[0], DATE_FORMAT)
+    data = float(row[1])
+    if dataSet == 'rec-center-hourly':
+      inputRecord = {
+        "timestamp": timestamp,
+        "kw_energy_consumption": data
+      }
+    elif dataSet == 'nyc_taxi':
+      inputRecord = {
+        "timestamp": row[0],
+        "passenger_count": float(row[1]),
+        # "timeofday": row[2],
+        # "dayofweek": row[3],
+      }
+
+    result = model.run(inputRecord)
+
+    tp = model._getTPRegion()
+    tm = tp.getSelf()._tfdr
+    tpOutput = tm.infActiveState['t']
+    patternNZ = tpOutput.reshape(-1).nonzero()[0]
+    activeColumn = patternNZ / tm.cellsPerColumn
+    activeColumnNum.append(len(activeColumn))
+
+    result.metrics = metricsManager.update(result)
+
+    negLL = result.metrics["multiStepBestPredictions:multiStep:"
+                             "errorMetric='negativeLogLikelihood':steps=5:window=1000:"
+                             "field="+predictedField]
+    if counter % 100 == 0 and counter>0:
+      negLL = result.metrics["multiStepBestPredictions:multiStep:"
+                             "errorMetric='negativeLogLikelihood':steps=5:window=1000:"
+                             "field="+predictedField]
+      nrmse = result.metrics["multiStepBestPredictions:multiStep:"
+                             "errorMetric='nrmse':steps=5:window=1000:"
+                             "field="+predictedField]
+
+      numActiveCell = np.mean(activeColumnNum[-100:])
+      print "After %i records, 5-step negLL=%f nrmse=%f meanActiveCell %f" % (counter, negLL, nrmse, numActiveCell)
+
+    if plot:
+      result = shifter.shift(result)
+
+    # prediction_1step = result.inferences["multiStepBestPredictions"][1]
+    last_prediction = prediction_5step
+    prediction_5step = result.inferences["multiStepBestPredictions"][5]
+
+
+    output.write([timestamp], [data], [float(prediction_5step)])
+
+
+
+    # bucketLL = result.inferences['multiStepBucketLikelihoods'][5]
+    # likelihoodsVec = np.zeros((maxBucket,))
+    # if bucketLL is not None:
+    #   for (k, v) in bucketLL.items():
+    #     likelihoodsVec[k] = v
+    #
+    # time_step.append(counter)
+    # actual_data.append(data)
+    # predict_data_ML.append(result.inferences['multiStepBestPredictions'][5])
+    # negLL_track.append(negLL)
+    #
+    # likelihoodsVecAll[0:len(likelihoodsVec), counter] = likelihoodsVec
+
+    counter += 1
+    startPlotFrom = 8000
+    if plot and counter > startPlotFrom:
+      # prepare data for display
+      if counter > 100:
+        time_step_display = time_step[-100:]
+        actual_data_display = actual_data[-100:]
+        predict_data_ML_display = predict_data_ML[-100:]
+        likelihood_display = likelihoodsVecAll[:, counter-100:counter]
+        xl = [(counter)-100, (counter)]
+      else:
+        time_step_display = time_step
+        actual_data_display = actual_data
+        predict_data_ML_display = predict_data_ML
+        likelihood_display = likelihoodsVecAll[:, :counter]
+        xl = [0, (counter)]
+
+      plt.figure(1)
+      plt.clf()
+      plt.imshow(likelihood_display, extent=(time_step_display[0], time_step_display[-1], 0, 40000),
+                 interpolation='nearest', aspect='auto', origin='lower', cmap='Reds')
+      plt.plot(time_step_display, actual_data_display, 'k', label='Data')
+      plt.plot(time_step_display, predict_data_ML_display, 'b', label='Best Prediction')
+      plt.xlim(xl)
+      plt.draw()
+
+
+
+  inputFile.close()
+  output.close()
