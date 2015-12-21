9870f3cdea194e488015e785dbefd4a29f6b6306
Sagan Bolliger <sbolliger@numenta.com>
2015-12-21 13:38:36 -0800

    Switch to using .getAlgorithmInstance() instead of accessing private member variable _knn directly.

--- htmresearch/frameworks/nlp/classify_htm.py
+++ htmresearch/frameworks/nlp/classify_htm.py
@@ -85,7 +85,7 @@ class ClassificationModelHTM(ClassificationModel):
 
 
   def getKNN(self):
-    return self.classifierRegion.getSelf()._knn
+    return self.classifierRegion.getSelf().getAlgorithmInstance()
 
 
   def prepData(self, dataPath, ordered=False, stripCats=False, **kwargs):
