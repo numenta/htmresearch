17aa1b66c6aae3b9b80a0f6ad4fbacb4be5cc464
Luiz Scheinkman <lscheinkman@numenta.com>
2016-03-29 13:48:20 -0700

    IMBU-117: Add progress and error indicator to results

--- projects/imbu/gui/browser/stores/search.js
+++ projects/imbu/gui/browser/stores/search.js
@@ -28,7 +28,9 @@ export default class SearchStore extends BaseStore {
   static storeName = 'SearchStore';
 
   static handlers = {
+    SEARCH_QUERY_MODEL: '_handleQueryModel',
     SEARCH_RECEIVED_DATA: '_handleReceivedData',
+    SEARCH_RECEIVED_DATA_ERROR: '_handleReceivedData',
     SEARCH_CLEAR_DATA: '_handleClearData'
   };
 
@@ -62,7 +64,58 @@ export default class SearchStore extends BaseStore {
    * @returns {array} current query results
    */
   getResults(model) {
-    return this.results.get(model) || [];
+    if (this.results.has(model)) {
+      return this.results.get(model).data || [];
+    }
+    return [];
+  }
+
+  /**
+   * Get status for the given model
+   * @param  {[type]} model [description]
+   * @returns {string} current status ("error", "ready", "pending") or null
+   */
+  getStatus(model) {
+    if (this.results.has(model)) {
+      return this.results.get(model).status;
+    }
+    return null;
+  }
+
+  /**
+   * Get error for the given model
+   * @param  {[type]} model [description]
+   * @returns {string} last error or null
+   */
+  getError(model) {
+    if (this.results.has(model)) {
+      return this.results.get(model).error;
+    }
+    return null;
+  }
+
+  /**
+   * Handle new query event
+   * @param  {object} payload New data
+   */
+  _handleQueryModel(payload) {
+    let {query, model} = payload;
+    this.query = query;
+    if (this.query) {
+      // Remove whitespaces
+      this.query = query.trim();
+      // Do not add empty queries to history
+      if (this.query.length > 0) {
+        this.history.add(this.query);
+      }
+    }
+    if (model) {
+      this.results.set(model, {status:'pending'});
+    } else {
+      // No model
+      this.results.clear();
+    }
+    this.emitChange();
   }
 
    /**
@@ -70,55 +123,52 @@ export default class SearchStore extends BaseStore {
     * @param  {object} payload New data
     */
   _handleReceivedData(payload) {
-    if (payload.query) {
-      // Remove whitespaces
-      this.query = payload.query.trim();
-    } else {
-      this.query = '';
+    let {query, model, results, error} = payload;
+
+    if (query !== this.query) {
+      this.results.set(model, {
+        status:'error',
+        error: `Unexpected query results: Query = ${query}.` +
+               `Expecting Query = ${this.query}`
+      });
+      this.emitChange();
+      return;
     }
 
-    // Do not add empty queries to history
-    if (this.query) {
-      this.history.add(this.query);
-    }
+    if (error) {
+      this.results.set(model, {status:'error', error});
+    } else if (results) {
+      // Find and sort results by max score
+      let data = Object.keys(results)
+        .map((id) => {
+          let record = results[id];
+          let text = record.text;
+          let scores = record.scores;
+          let windowSize = record.windowSize;
 
-    if (payload.model) {
-      let model = payload.model;
-      if (payload.results) {
-        // Find and sort results by max score
-        let records = Object.keys(payload.results)
-          .map((id) => {
-            let record = payload.results[id];
-            let text = record.text;
-            let scores = record.scores;
-            let windowSize = record.windowSize;
-
-            // Find max
-            let maxScore = record.scores.reduce((prev, current) => {
-              return prev > current ? prev : current;
-            });
-            let sumScore = record.scores.reduce((prev, current) => {
-              return prev + current ;
-            });
-            return {
-              text, maxScore, sumScore, scores, windowSize
-            };
-          })
-          .sort((a, b) => {
-            let res = b.maxScore - a.maxScore;
-            if (res === 0) {
-              res = b.sumScore - a.sumScore;
-            }
-            return res;
+          // Find max
+          let maxScore = record.scores.reduce((prev, current) => {
+            return prev > current ? prev : current;
           });
-        this.results.set(model, records);
-      } else {
-        // No data
-        this.results.delete(model);
-      }
+          let sumScore = record.scores.reduce((prev, current) => {
+            return prev + current ;
+          });
+          return {
+            text, maxScore, sumScore, scores, windowSize
+          };
+        })
+        .sort((a, b) => {
+          let res = b.maxScore - a.maxScore;
+          if (res === 0) {
+            res = b.sumScore - a.sumScore;
+          }
+          return res;
+        });
+      this.results.set(model, {status:'ready', data});
     } else {
-      // No model
-      this.results.clear();
+      // No data
+      this.results.delete(model);
+      this.results.set(model, {status:'ready'});
     }
     this.emitChange();
   }
