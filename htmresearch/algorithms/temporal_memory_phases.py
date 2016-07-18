ba1425565a2834c842d88d7fb56244b42b4e8f4d
Andrew Malta <andrew.malta@yale.edu>
2016-07-18 14:12:40 -0700

    fixes typo and updates file header

--- htmresearch/algorithms/temporal_memory_phases.py
+++ htmresearch/algorithms/temporal_memory_phases.py
@@ -1,6 +1,6 @@
 # ----------------------------------------------------------------------
 # Numenta Platform for Intelligent Computing (NuPIC)
-# Copyright (C) 2014, Numenta, Inc.  Unless you have an agreement
+# Copyright (C) 2014-2016, Numenta, Inc.  Unless you have an agreement
 # with Numenta, Inc., for a separate license for this software code, the
 # following terms and conditions apply:
 #
@@ -20,7 +20,7 @@
 # ----------------------------------------------------------------------
 
 """
-A deprectated version of Temporal Memory implementation in Python. Uses
+A deprecated version of Temporal Memory implementation in Python. Uses
 the 4 phases as described in the BAMI Pseudocode.  An updated version
 of temporal memory lives in github.com/numenta/nupic. They do not operate
 in the exact same way because calls to the random number generator
