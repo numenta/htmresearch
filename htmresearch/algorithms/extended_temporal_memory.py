4cc7270c4a96be8e4105760e2873d2096820d675
natoromano <nathanaelpromano@gmail.com>
2016-07-18 09:49:41 -0700

    Fixed reindexing bug in ETM.

--- htmresearch/algorithms/extended_temporal_memory.py
+++ htmresearch/algorithms/extended_temporal_memory.py
@@ -86,9 +86,8 @@ class ExtendedTemporalMemory(TemporalMemory):
     if activeApicalCells is None:
       activeApicalCells = set()
 
-    activeExternalCells = self._reindexActiveExternalCells(activeExternalCells)
-    activeApicalCells = self._reindexActiveApicalCells(activeApicalCells,
-                                                       len(activeExternalCells))
+    activeExternalCells = self._reindexActiveCells(activeExternalCells)
+    activeApicalCells = self._reindexActiveCells(activeApicalCells)
 
     (activeCells,
      winnerCells,
@@ -505,22 +504,15 @@ class ExtendedTemporalMemory(TemporalMemory):
     return self.predictedActiveCells
 
 
-  def _reindexActiveExternalCells(self, activeExternalCells):
+  def _reindexActiveCells(self, activeCells):
     """
-    Move sensorimotor input indices to outside the range of valid cell indices
+    Move sensorimotor or apical input indices to outside the range of valid
+    cell indices
 
-    @params activeExternalCells (set) Indices of active external cells in `t`
+    @params activeCells (set) Indices of active external cells in `t`
     """
     numCells = self.numberOfCells()
-    return set([index + numCells for index in activeExternalCells])
-
-
-  def _reindexActiveApicalCells(self, activeApicalCells, numExtCells):
-    """
-    @params activeApicalCells (set) Indices of active apical cells in `t`
-    """
-    numCells = self.numberOfCells() + numExtCells
-    return set([index + numCells for index in activeApicalCells])
+    return set([index + numCells for index in activeCells])
 
 
   def calculatePredictiveCells(self, predictiveDistalCells,
