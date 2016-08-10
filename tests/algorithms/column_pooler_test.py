0b74fc24fb5e68060d4db5da8e4a4805cc6a844d
natoromano <nathanaelpromano@gmail.com>
2016-08-10 15:52:09 -0700

    Stable set-up

--- tests/algorithms/column_pooler_test.py
+++ tests/algorithms/column_pooler_test.py
@@ -24,94 +24,180 @@ import numpy as np
 
 import scipy.sparse as sparse
 
+from nupic.data.generators.pattern_machine import PatternMachine
 from nupic.research.monitor_mixin.temporal_memory_monitor_mixin import (
-	TemporalMemoryMonitorMixin
-	)
+  TemporalMemoryMonitorMixin
+)
 from htmresearch.algorithms.column_pooler import ColumnPooler, realDType
 
 
-class MonitoredColumnPooler(TemporalMemoryMonitorMixin, ColumnPooler): 
-	pass
+
+class MonitoredColumnPooler(TemporalMemoryMonitorMixin, ColumnPooler):
+  pass
 
 
-class ExtensiveColumnPoolerTest(unittest.Testcase):
-	"""Algorithmic tests for the ColumnPooler region."""
+class ExtensiveColumnPoolerTest(unittest.TestCase):
+  """Algorithmic tests for the ColumnPooler region."""
 
-	inputWidth = 2048
-	inputActive = 40
-	outputWidth = 2048
-	outputActive = 40
-	seed = 42
-
-
-	def setUp(self):
-		self.pooler = None
-		self.patternMachine = PatternMachine(
-			self.inputWidth, 
-			self.outputActive, 
-			self.seed
-			)
-		np.seed(self.seed)
+  inputWidth = 2048 * 8
+  numInputActiveBits = 0.02 * inputWidth
+  outputWidth = 2048
+  numOutputActiveBits = 40
+  seed = 42
 
 
-	def init(self, overrides=None):
-		params = self._computeParams(overrides)
-		self.pooler = MonitoredColumnPooler(params)
+  def testNewInputs(self):
+    self.init()
+
+    # feed the first input, a random SDR should be generated
+    firstPattern = self.getObject(1)
+    self.learn(firstPattern, numRepetitions=1, newObject=True)
+    currentRespresentation = set(self.pooler.getActiveCells())
+    self.assertEqual(len(currentRespresentation), self.numOutputActiveBits)
+
+    # feed a new input for the same object, the previous SDR should persist
+    secondPattern = self.getObject(1)
+    self.learn(secondPattern, numRepetitions=1, newObject=False)
+    newRepresentation = set(self.pooler.getActiveCells())
+    self.assertNotEqual(firstPattern, secondPattern)
+    self.assertEqual(newRepresentation, currentRespresentation)
+
+    # without sensory input, the SDR should persist as well
+    self.learn([None], numRepetitions=1, newObject=False)
+    newRepresentation = set(self.pooler.getActiveCells())
+    self.assertEqual(newRepresentation, currentRespresentation)
 
 
-	def getDefaultPoolerParams(self):
-	  return {
-	  	"inputWidth": self.inputWidth,
-	  	"numActivecolumnsPerInhArea": self.outputActive,
-	  	"synPermProximalInc": 0.1,
-	  	"synPermProximalDec": 0.001,
-	    "columnDimensions": (self.outputWidth,),
-	    "cellsPerColumn": 32,
-	    "initialPermanence": 0.5,
-	    "connectedPermanence": 0.6,
-	    "minThreshold": 25,
-	    "maxNewSynapseCount": 30,
-	    "permanenceIncrement": 0.1,
-	    "permanenceDecrement": 0.02,
-	    "predictedSegmentDecrement": 0.08,
-	    "activationThreshold": 25,
-	    "seed": self.seed,
-	    "learnOnOneCell": False,
-	  }
+  def setUp(self):
+    """
+    Sets up the test.
+    """
+    self.pooler = None
+    self.proximalPatternMachine = PatternMachine(
+      self.inputWidth,
+      self.numOutputActiveBits,
+      self.seed
+    )
+    self.patternId = 0
+    np.random.seed(self.seed)
 
 
-	def learnObject(self,
-									feedforwardPatterns,
-									lateralPatterns=None, 
-									numRepetitions=1, 
-									randomOrder=True):
-		self.pooler.mmClearHistory()
-		self.pooler.reset()
-
-		# set-up
-		indices = range(len(feedforwardPatterns))
-		if lateralPatterns is None:
-			lateralPatterns = [None] * len(feedforwardPatterns)
-
-		for _ in xrange(numRepetitions):
-			if randomOrder:
-				np.random.shuffle(indices)
-
-			for idx in indices:
-				self.pooler.compute(feedforwardInput=feedforwardPatterns[idx], 
-														activeExternalCells=lateralPatterns[idx],
-														learn=True)
+  def getPattern(self):
+    """
+    Returns a random proximal input pattern.
+    """
+    pattern = self.proximalPatternMachine.get(self.patternId)
+    self.patternId += 1
+    return pattern
 
 
-	def infer(self, feedforwardPattern, lateralPattern, printMetrics=False):
+  def learn(self,
+            feedforwardPatterns,
+            lateralPatterns=None,
+            numRepetitions=1,
+            randomOrder=True,
+            newObject=True):
+    """
+    Learns a single object, with the provided patterns.
+
+    @feedforwardPatterns   (list(set)) List of proximal input patterns
+    @lateralPatterns       (list(set)) List of distal input patterns, or None
+                                       if no lateral input is used. This is
+                                       expected to have the same length as
+                                       feedforwardPatterns
+    @numRepetitions        (int)       Number of times the patterns will be fed
+    @randomOrder           (bool)      If true, the order of patterns will be
+                                       shuffled at each repetition
+    """
+    if newObject:
+      self.pooler.mmClearHistory()
+      self.pooler.reset()
+
+    # set-up
+    indices = range(len(feedforwardPatterns))
+    if lateralPatterns is None:
+      lateralPatterns = [None] * len(feedforwardPatterns)
+
+    for _ in xrange(numRepetitions):
+      if randomOrder:
+        np.random.shuffle(indices)
+
+      for idx in indices:
+        self.pooler.compute(feedforwardPatterns[idx],
+                            activeExternalCells=lateralPatterns[idx],
+                            learn=True)
 
 
+  def infer(self, feedforwardPattern, lateralPattern=None, printMetrics=False):
+    """
+    Feeds a single pattern to the column pooler (as well as an eventual lateral
+    pattern).
 
-	def _computeParams(self, overrides):
-		params = self.getDefaultPoolerParams()
-		params.update(overrides)
-		return params
+    @param feedforwardPattern       (set) Input proximal pattern to the pooler
+    @param lateralPattern           (set) Input dislal pattern to the pooler
+    @param printMetrics             (bool) If true, will print cell metrics
+    """
+    self.pooler.compute(feedforwardPattern,
+                        activeExternalCells=lateralPattern,
+                        learn=False)
+
+    if printMetrics:
+      print self.pooler.mmPrettyPrintMetrics(self.pooler.mmGetDefaultMetrics())
+
+
+  def getObject(self, numPatterns):
+    """
+    Creates a list of patterns, for a given object.
+    """
+    return [self.getPattern() for _ in xrange(numPatterns)]
+
+
+  def init(self, overrides=None):
+    """
+    Creates the column pooler with specified parameter overrides.
+    """
+    params = self._computeParams(overrides)
+    self.pooler = MonitoredColumnPooler(**params)
+
+
+  def getDefaultPoolerParams(self):
+    """
+    Default params to be used for the column pooler, if no override is
+    specified.
+    """
+    return {
+      "inputWidth": self.inputWidth,
+      "numActivecolumnsPerInhArea": self.numOutputActiveBits,
+      "synPermProximalInc": 0.1,
+      "synPermProximalDec": 0.001,
+      "maxSynapsesPerSegment": self.inputWidth,
+      "columnDimensions": (self.outputWidth,),
+      "initialPermanence": 0.5,
+      "connectedPermanence": 0.6,
+      "minThreshold": 20,
+      "maxNewSynapseCount": 30,
+      "permanenceIncrement": 0.1,
+      "permanenceDecrement": 0.02,
+      "predictedSegmentDecrement": 0.08,
+      "activationThreshold": 20,
+      "seed": self.seed,
+      "learnOnOneCell": False,
+    }
+
+
+  def _computeParams(self, overrides):
+    """
+    Overrides the default parameters with provided values and returns the
+    parameters to use in the constructor.
+    """
+    if overrides is None:
+      overrides = {}
+
+    params = self.getDefaultPoolerParams()
+    params.update(overrides)
+    return params
+
 
 
 if __name__ == "__main__":
-	unittest.main()
+  unittest.main()
