b64c16a8ca8272f95acd51c3ce638a52ac0941c0
Yuwei Cui <ycui@numenta.com>
2016-08-30 11:56:52 -0700

    Add Entropy Metric to SP monitor

--- htmresearch/support/spatial_pooler_monitor_mixin.py
+++ htmresearch/support/spatial_pooler_monitor_mixin.py
@@ -22,13 +22,17 @@
 """
 Spatial Pooler mixin that enables detailed monitoring of history.
 """
-
+import copy
 from collections import defaultdict
 import numpy
 from nupic.research.monitor_mixin.metric import Metric
 from nupic.research.monitor_mixin.monitor_mixin_base import MonitorMixinBase
 from nupic.research.monitor_mixin.trace import (
-  IndicesTrace, CountsTrace)
+  IndicesTrace, CountsTrace, BoolsTrace, MetricsTrace)
+from nupic.bindings.math import GetNTAReal
+
+realDType = GetNTAReal()
+uintType = "uint32"
 
 
 
@@ -59,6 +63,20 @@ class SpatialPoolerMonitorMixin(MonitorMixinBase):
     return self._mmTraces["numConnections"]
 
 
+  def mmGetTraceLearn(self):
+    """
+    @return (Trace) Trace of learn flags
+    """
+    return self._mmTraces["learn"]
+
+
+  def mmGetDataInitialPermanence(self):
+    """
+    @return (array) Initial permamnences
+    """
+    return self._mmData["initialPermanence"]
+
+
   def mmGetDataDutyCycles(self):
     """
     @return (list) duty cycles for all columns
@@ -83,6 +101,19 @@ class SpatialPoolerMonitorMixin(MonitorMixinBase):
                   dutyCycles)
 
 
+  def mmGetMetricEntropy(self):
+    """
+    @return (Metric) entropy 
+    """
+    dutyCycles = self.mmGetDataDutyCycles()
+    MIN_ACTIVATION_PROB = 0.000001
+
+    dutyCycles[dutyCycles < MIN_ACTIVATION_PROB] = MIN_ACTIVATION_PROB
+    dutyCycles = dutyCycles / numpy.sum(dutyCycles)
+
+    entropy = -numpy.dot(dutyCycles, numpy.log2(dutyCycles))
+    return Metric(self, "entropy", entropy)
+  
   # ==============================
   # Overrides
   # ==============================
@@ -95,13 +126,15 @@ class SpatialPoolerMonitorMixin(MonitorMixinBase):
     activeColumns = numpy.nonzero(activeArray)
 
     # total number of connections
-    connectedCounts = numpy.zeros(self._numColumns, dtype='uint32')
+    connectedCounts = numpy.zeros(self._numColumns, dtype=uintType)
     self.getConnectedCounts(connectedCounts)
     numConnections = numpy.sum(connectedCounts)
+        
 
     self._mmTraces["activeInputs"].data.append(numpy.where(inputVector)[0])
     self._mmTraces["activeColumns"].data.append(activeColumns)
     self._mmTraces["numConnections"].data.append(numConnections)
+    self._mmTraces["learn"].data.append(learn)    
 
 
   def mmGetDefaultTraces(self, verbosity=1):
@@ -131,3 +164,78 @@ class SpatialPoolerMonitorMixin(MonitorMixinBase):
     self._mmTraces["activeColumns"] = IndicesTrace(self, "active columns")
     self._mmTraces["activeInputs"] = IndicesTrace(self, "active inputs")
     self._mmTraces["numConnections"] = CountsTrace(self, "connections")
+    self._mmTraces["learn"] = BoolsTrace(self, "learn")    
+
+    initialPermanence = numpy.zeros((self._numColumns, self._numInputs),
+                                    dtype=realDType)
+    for c in range(self._numColumns):
+      self.getPermanence(c, initialPermanence[c, :])
+
+    self._mmData["initialPermanence"] = initialPermanence
+
+
+
+  def recoverPermanence(self, columnIndex):
+    """
+    Recover permamnece for a single column
+    :param columnIndex: index of the column of interest
+    :return: permInfo (dict) with the following traces over time
+              avgPermConnectedSyn: mean permanence for connected synapses
+              avgPermNonConnectedSyn: mean permanence for non-connected synapses
+              numConnectedSyn: number of connected synapses
+              numNonConnectedSyn: number of non-connected synapses
+    """
+    activeColumns = self.mmGetTraceActiveColumns()
+    activeInputs = self.mmGetTraceActiveInputs()
+    learnTrace = self.mmGetTraceLearn()
+    initialPermanence = self.mmGetDataInitialPermanence()
+
+    (columnNumber, inputSize) = initialPermanence.shape
+
+    # potential mask for the column of interest
+    potential = numpy.zeros((inputSize), dtype=uintType)
+    self.getPotential(columnIndex, potential)
+    maskPotential = numpy.where(potential > 0)[0]
+
+    permChanges = numpy.zeros(inputSize, dtype=realDType)
+    perm = numpy.copy(initialPermanence[columnIndex, :])
+
+    numStep = len(activeColumns.data)
+    numConnectedSyn = numpy.zeros((numStep))
+    avgPermConnected = numpy.zeros((numStep))
+    avgPermNonConnected = numpy.zeros((numStep))
+
+    for i in range(numStep):
+      if learnTrace.data[i] is False:
+        continue
+
+      if columnIndex in activeColumns.data[i][0]:
+        # print "Column {} active at step {}".format(columnIndex, i)
+        # the logic here matches _adaptSynapses funciton in SP
+        permChanges.fill(-1 * self._synPermInactiveDec)
+        permChanges[activeInputs.data[i]] = self._synPermActiveInc
+        perm[maskPotential] += permChanges[maskPotential]
+
+        perm[perm < self._synPermTrimThreshold] = 0
+        numpy.clip(perm, self._synPermMin, self._synPermMax, out=perm)
+
+      numConnectedSyn[i] = numpy.sum(perm > self._synPermConnected)
+      permMask = perm[maskPotential]
+      avgPermConnected[i] = numpy.mean(permMask[
+                                         permMask > self._synPermConnected])
+      avgPermNonConnected[i] = numpy.mean(permMask[
+                                            permMask < self._synPermConnected])
+
+    truePermanence = numpy.zeros((inputSize), dtype=realDType)
+    self.getPermanence(columnIndex, truePermanence)
+    if numpy.max(numpy.abs(perm - truePermanence)) > 1e-10:
+      raise RuntimeError("Permamnence reconstruction failed. The reconstructed"
+                         "permamnence does not match the real permamnence")
+
+    numNonConnectedSyn = len(maskPotential) - numConnectedSyn
+    permInfo = {"avgPermConnectedSyn": avgPermConnected,
+                "avgPermNonConnectedSyn": avgPermNonConnected,
+                "numConnectedSyn": numConnectedSyn,
+                "numNonConnectedSyn": numNonConnectedSyn}
+
+    return permInfo
