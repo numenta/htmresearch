89edddc2fe3349c41ce9d47cc5e437641d2f84cb
Austin Marshall <amarshall@numenta.com>
2016-09-15 10:10:45 -0700

    Refactored faulty temporal memory implementation to use current Python temporal memory from nupic.

--- htmresearch/algorithms/faulty_temporal_memory.py
+++ htmresearch/algorithms/faulty_temporal_memory.py
@@ -23,25 +23,109 @@
 Faulty Temporal Memory implementation in Python.
 """
 import numpy
-from collections import defaultdict
 from htmresearch.algorithms.temporal_memory_phases import TemporalMemory
+from nupic.research.temporal_memory import  TemporalMemory
+from nupic.research.connections import CellData, Connections
+
+
+
+class FaultyCellData(CellData):
+  """ CellData subclass that adds a `destroyed` property that evaluates to True
+  when a cell is "killed"
+  """
+
+
+  __slots__ = CellData.__slots__ + ["__destroyed"]
+
+
+  def __init__(self):
+    super(FaultyCellData, self).__init__()
+    self.__destroyed = False
+
+
+  @property
+  def destroyed(self):
+    return self.__destroyed
+
+
+  def destroy(self):
+    self.__destroyed = True # Destruction is permanent!
+
+
+
+class FaultyConnections(Connections):
+  def __init__(self,
+               numCells,
+               maxSegmentsPerCell=255,
+               maxSynapsesPerSegment=255):
+    super(FaultyConnections, self).__init__(
+      numCells=numCells,
+      maxSegmentsPerCell=maxSegmentsPerCell,
+      maxSynapsesPerSegment=maxSynapsesPerSegment)
+
+    # Redo _cells assignment to use FaultyCellData, which supports destruction
+    # of cells
+    self._cells = [FaultyCellData() for _ in xrange(numCells)]
+
+
+  @property
+  def cells(self):
+    # Expose otherwise internal _cells as a property
+    return self._cells
+
+
+  def destroyCell(self, cellIdx):
+
+    # Destroy cell
+    self.cells[cellIdx].destroy()
+
+    # Destroy segments.  self.destroySegment() takes care of deleting synapses
+    segmentCount = 0
+    for segment in self.segmentsForCell(cellIdx):
+      self.destroySegment(segment)
+      segmentCount += 1
+
+    return segmentCount
+
+
+  def segmentsForCell(self, cell):
+    # Completely ignore segments for destroyed cells
+    if self.cells[cell].destroyed:
+      return iter([])
+
+    return super(FaultyConnections, self).segmentsForCell(cell)
+
+
+  def synapsesForSegment(self, segment):
+    # Completely ignore synapses on destroyed cells and destroyed segments
+    if self.cells[segment.cell].destroyed or segment._destroyed:
+      return iter([])
+
+    # Return only synapses connected to non-destroyed presynaptic cells
+    return (synapse
+            for synapse
+            in super(FaultyConnections, self).synapsesForSegment(segment)
+            if not self.cells[synapse.presynapticCell].destroyed)
+
+
+  def createSynapse(self, segment, presynapticCell, permanence):
+    # Do NOT create synapses on dead cells/segments or to dead presynaptic cells
+    if (self.cells[segment.cell].destroyed or
+        self.cells[presynapticCell].destroyed or
+        segment._destroyed):
+      return
+
+    return (super(FaultyConnections, self)
+            .createSynapse(segment, presynapticCell, permanence))
+
+
 
 class FaultyTemporalMemory(TemporalMemory):
   """
   Class implementing a fallible Temporal Memory class. This class allows the
   user to kill a certain number of cells. The dead cells cannot become active,
   will no longer participate in predictions, and cannot become winning cells.
-  While admittedly cruel and cold hearted, this feature  enables us to test the
-  robustness of the Temporal Memory algorithm under such situations. Such is the
-  price of progress.
-
-  And by the way, we're not actually killing anything real.
   """
-
-  # ==============================
-  # Main functions
-  # ==============================
-
 
   def __init__(self,
                **kwargs):
@@ -52,12 +136,26 @@ class FaultyTemporalMemory(TemporalMemory):
     self.numDead = 0
 
 
+  @staticmethod
+  def connectionsFactory(*args, **kwargs):
+    """
+    Override connectionsFactory() in base class to return a Connections subclass
+    that supports cell destruction.
+
+    See Connections for constructor signature and usage
+
+    @return: FaultyConnections instance
+    """
+    return FaultyConnections(*args, **kwargs)
+
+
   def killCells(self, percent = 0.05):
     """
     Changes the percentage of cells that are now considered dead. The first
     time you call this method a permutation list is set up. Calls change the
     number of cells considered dead.
     """
+
     if self.zombiePermutation is None:
       self.zombiePermutation = numpy.random.permutation(self.numberOfCells())
 
@@ -70,206 +168,139 @@ class FaultyTemporalMemory(TemporalMemory):
     print "Total number of dead cells=",len(self.deadCells)
 
     numSegmentDeleted = 0
-    for cell in self.deadCells:
-      segmentsPerCell = list(self.connections.segmentsForCell(cell))
-      for segment in segmentsPerCell:
-        self.connections.destroySegment(segment)
-        numSegmentDeleted += 1
+
+    for cellIdx in self.deadCells:
+      numSegmentDeleted += self.connections.destroyCell(cellIdx)
 
     print "Total number of segments removed=", numSegmentDeleted
 
 
-  def activateCorrectlyPredictiveCells(self,
-                                       prevPredictiveCells,
-                                       prevMatchingCells,
-                                       activeColumns):
+  def activatePredictedColumn(self, *args, **kwargs):
+    # Return only non-destroyed cells
+    return [idx
+            for idx
+            in TemporalMemory.activatePredictedColumn(*args, **kwargs)
+            if not self.connections.cells[idx].destroyed]
+
+
+  def activateDendrites(self, learn=True):
+    """ Originally copied from temporal_memory.py and augmented to suppress
+    segments from dead cells.  See TemporalMemory.activateDendrites() for
+    original implementation.
     """
-    Phase 1: Activate the correctly predictive cells.
+    (numActiveConnected,
+     numActivePotential) = self.connections.computeActivity(
+      self.activeCells,
+      self.connectedPermanence)
 
-    Pseudocode:
+    # Suppress segments from dead cells
+    activeSegments = (
+      self.connections.segmentForFlatIdx(i)
+      for i in xrange(len(numActiveConnected))
+      if numActiveConnected[i] >= self.activationThreshold and
+         not self.connections.cells[
+           self.connections.segmentForFlatIdx(i).cell].destroyed
+    )
 
-      - for each prev predictive cell
-        - if in active column
-          - mark it as active
-          - mark it as winner cell
-          - mark column as predicted => active
-        - if not in active column
-          - mark it as an predicted but inactive cell
+    # Suppress segments from dead cells
+    matchingSegments = (
+      self.connections.segmentForFlatIdx(i)
+      for i in xrange(len(numActivePotential))
+      if numActivePotential[i] >= self.minThreshold and
+         not self.connections.cells[self.connections.segmentForFlatIdx(i).cell].destroyed
+    )
 
-    @param prevPredictiveCells (set) Indices of predictive cells in `t-1`
-    @param activeColumns       (set) Indices of active columns in `t`
+    maxSegmentsPerCell = self.connections.maxSegmentsPerCell
+    segmentKey = lambda segment: (segment.cell * maxSegmentsPerCell
+                                  + segment.idx)
+    self.activeSegments = sorted(activeSegments, key=segmentKey)
+    self.matchingSegments = sorted(matchingSegments, key=segmentKey)
+    self.numActiveConnectedSynapsesForSegment = numActiveConnected
+    self.numActivePotentialSynapsesForSegment = numActivePotential
 
-    @return (tuple) Contains:
-                      `activeCells`               (set),
-                      `winnerCells`               (set),
-                      `predictedActiveColumns`    (set),
-                      `predictedInactiveCells`    (set)
+    if learn:
+      for segment in self.activeSegments:
+        self.connections.recordSegmentActivity(segment)
+      self.connections.startNewIteration()
+
+
+  def burstColumn(self, connections, random, column, columnMatchingSegments,
+                  prevActiveCells, prevWinnerCells,
+                  numActivePotentialSynapsesForSegment, cellsPerColumn,
+                  maxNewSynapseCount, initialPermanence, permanenceIncrement,
+                  permanenceDecrement, learn):
+    """ Originally copied from temporal_memory.py and augmented to ignore dead
+    cells.  See TemporalMemory.burstColumns() for original implementation.
     """
-    activeCells = set()
-    winnerCells = set()
-    predictedActiveColumns = set()
-    predictedInactiveCells = set()
+    start = cellsPerColumn * column
+    cells = xrange(start, start + cellsPerColumn)
 
-    prevPredictiveCells = prevPredictiveCells - self.deadCells
-    prevMatchingCells = prevMatchingCells - self.deadCells
+    # Strip out destroyed cells
+    cells = [cellIdx
+             for cellIdx
+             in cells
+             if not self.connections.cells[cellIdx].destroyed]
 
-    for cell in prevPredictiveCells:
-      column = self.columnForCell(cell)
+    if columnMatchingSegments is not None:
+      numActive = lambda s: numActivePotentialSynapsesForSegment[s.flatIdx]
 
-      if column in activeColumns:
-        activeCells.add(cell)
-        winnerCells.add(cell)
-        predictedActiveColumns.add(column)
+      bestMatchingSegment = None
+      for seg in columnMatchingSegments:
+        if bestMatchingSegment is None:
+          bestMatchingSegment = seg
+        elif seg > bestMatchingSegment and not connections.cells[seg.cell].destroyed:
+          bestMatchingSegment = seg
 
-    if self.predictedSegmentDecrement > 0:
-      for cell in prevMatchingCells:
-        column = self.columnForCell(cell)
+      winnerCell = bestMatchingSegment.cell
 
-        if column not in activeColumns:
-          predictedInactiveCells.add(cell)
+      if learn:
+        self.adaptSegment(connections, bestMatchingSegment, prevActiveCells,
+                         permanenceIncrement, permanenceDecrement)
 
-    return (activeCells,
-            winnerCells,
-            predictedActiveColumns,
-            predictedInactiveCells)
+        nGrowDesired = maxNewSynapseCount - numActive(bestMatchingSegment)
 
+        if nGrowDesired > 0:
+          self.growSynapses(connections, random, bestMatchingSegment,
+                           nGrowDesired, prevWinnerCells, initialPermanence)
+    else:
+      winnerCell = self.leastUsedCell(random, cells, connections)
+      if learn:
+        nGrowExact = min(maxNewSynapseCount, len(prevWinnerCells))
+        if nGrowExact > 0:
+          segment = connections.createSegment(winnerCell)
+          self.growSynapses(connections, random, segment, nGrowExact,
+                           prevWinnerCells, initialPermanence)
 
-  def computePredictiveCells(self, activeCells, connections):
-    """
-    Phase 4: Compute predictive and matching cells due to lateral input
-    on distal dendrites.
-
-    Pseudocode:
-
-      - for each distal dendrite segment with activity >= activationThreshold
-        - mark the segment as active
-        - mark the cell as predictive
-
-      - for each distal dendrite segment with unconnected
-        activity >=  minThreshold
-        - mark the segment as matching
-        - mark the cell as matching
-
-    Forward propagates activity from active cells to the synapses that touch
-    them, to determine which synapses are active.
-
-    @param activeCells (set)         Indices of active cells in `t`
-    @param connections (Connections) Connectivity of layer
-
-    @return (tuple) Contains:
-                      `activeSegments`   (set),
-                      `predictiveCells`  (set),
-                      `matchingSegments` (set),
-                      `matchingCells`    (set)
-    """
-    numActiveConnectedSynapsesForSegment = defaultdict(lambda: 0)
-    numActiveSynapsesForSegment = defaultdict(lambda: 0)
-    activeSegments = set()
-    predictiveCells = set()
-
-    matchingSegments = set()
-    matchingCells = set()
-
-    for cell in activeCells:
-      assert(not (cell in self.deadCells))
-      for synapseData in connections.synapsesForPresynapticCell(cell).values():
-        segment = synapseData.segment
-        permanence = synapseData.permanence
-        postSynapticCell = connections.cellForSegment(segment)
-
-        if not(postSynapticCell in self.deadCells):
-          if permanence >= self.connectedPermanence:
-            numActiveConnectedSynapsesForSegment[segment] += 1
-
-            if (numActiveConnectedSynapsesForSegment[segment] >=
-                self.activationThreshold):
-              activeSegments.add(segment)
-              predictiveCells.add(connections.cellForSegment(segment))
-
-          if permanence > 0 and self.predictedSegmentDecrement > 0:
-            numActiveSynapsesForSegment[segment] += 1
-
-            if numActiveSynapsesForSegment[segment] >= self.minThreshold:
-              matchingSegments.add(segment)
-              matchingCells.add(connections.cellForSegment(segment))
+    return cells, winnerCell
 
 
-    return activeSegments, predictiveCells, matchingSegments, matchingCells
+  @classmethod
+  def growSynapses(cls, connections, random, segment, nDesiredNewSynapes,
+                   prevWinnerCells, initialPermanence):
+    # Do not allow synapses to be grown on destroyed cells
+    if connections.cells[segment.cell].destroyed:
+      return
+
+    # Ignore destroyed prevWinnerCells
+    prevWinnerCells = (prevWinnerCell
+                       for prevWinnerCell
+                       in prevWinnerCells
+                       if not connections.cells[prevWinnerCell].destroyed)
+
+    TemporalMemory.growSynapses(connections, random, segment,
+                                nDesiredNewSynapes, prevWinnerCells,
+                                initialPermanence)
 
 
-  def burstColumns(self,
-                   activeColumns,
-                   predictedActiveColumns,
-                   prevActiveCells,
-                   prevWinnerCells,
-                   connections):
-    """
-    Phase 2: Burst unpredicted columns.
+  @classmethod
+  def leastUsedCell(cls, random, cells, connections):
+    # Strip out destroyed cells for consideration
+    cells = [cellIdx
+             for cellIdx
+             in cells
+             if not connections.cells[cellIdx].destroyed]
 
-    Pseudocode:
-
-      - for each unpredicted active column
-        - mark all cells as activex
-        - If learnOnOneCell, keep the old best matching cell if it exists
-        - mark the best matching cell as winner cell
-          - (learning)
-            - if it has no matching segment
-              - (optimization) if there are prev winner cells
-                - add a segment to it
-            - mark the segment as learning
-
-    @param activeColumns                   (set)        Indices of active
-                                                        columns in `t`
-    @param predictedActiveColumns          (set)        Indices of predicted
-                                                        columns in `t`
-    @param prevActiveCells                 (set)        Indices of active cells
-                                                        in `t-1`
-    @param prevWinnerCells                 (set)        Indices of winner cells
-                                                        in `t-1`
-    @param connections                     (Connections) Connectivity of layer
-
-    @return (tuple) Contains:
-                      `activeCells`      (set),
-                      `winnerCells`      (set),
-                      `learningSegments` (set)
-    """
-    activeCells = set()
-    winnerCells = set()
-    learningSegments = set()
-
-    unpredictedActiveColumns = activeColumns - predictedActiveColumns
-
-    for column in unpredictedActiveColumns:
-      cells = self.cellsForColumn(column) - self.deadCells
-      if len(cells) == 0:
-        continue
-
-      activeCells.update(cells)
-
-      # if learnOnOneCell and (column in chosenCellForColumn):
-      #   chosenCell = chosenCellForColumn[column]
-      #   cells = set([chosenCell])
-
-      (bestCell,
-       bestSegment) = self.bestMatchingCell(cells,
-                                            prevActiveCells,
-                                            connections)
-      winnerCells.add(bestCell)
-
-      if bestSegment is None and len(prevWinnerCells):
-        bestSegment = connections.createSegment(bestCell)
-
-      if bestSegment is not None:
-        learningSegments.add(bestSegment)
-
-      # chosenCellForColumn[column] = bestCell
-
-    return activeCells, winnerCells, learningSegments
-
-
-  #########################################################################
-  #
-  # Debugging routines
+    return TemporalMemory.leastUsedCell(random, cells, connections)
 
 
   def printDeadCells(self):
@@ -281,13 +312,13 @@ class FaultyTemporalMemory(TemporalMemory):
       col = self.columnForCell(cell)
       columnCasualties[col] += 1
     for col in range(self.numberOfColumns()):
-      print col,columnCasualties[col]
+      print col, columnCasualties[col]
 
 
   def printSegmentsForCell(self, cell):
     segments = self.connections.segmentsForCell(cell)
 
-    print "Segments for cell",cell
+    print "Segments for cell", cell
     for segment in segments:
       self.printSegment(segment, self.connections)
 
@@ -305,5 +336,3 @@ class FaultyTemporalMemory(TemporalMemory):
       presynapticCell = synapseData.presynapticCell
       print "%d:%g" % (presynapticCell,permanence),
     print
-
-
