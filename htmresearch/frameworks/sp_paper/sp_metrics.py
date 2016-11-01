correct entropy calculation based on feedback from Mirko

htmresearch/frameworks/sp_paper/sp_metrics.py
@@ -718,7 +718,8 @@ def calculateEntropy(activeColumns):
   activationProb[activationProb < MIN_ACTIVATION_PROB] = MIN_ACTIVATION_PROB
   activationProb = activationProb / np.sum(activationProb)
 
-  entropy = -np.dot(activationProb, np.log2(activationProb))
+  entropy = -(np.dot(activationProb, np.log2(activationProb)) +
+              np.dot(1 - activationProb, np.log2(1-activationProb)))
   return entropy
 
 

projects/sp_paper/plot_noise_robustness.py
@@ -22,10 +22,13 @@
 
 from htmresearch.support.sp_paper_utils import *
 
-expName = 'randomSDRVaryingSparsity'
+import matplotlib as mpl
+mpl.rcParams['pdf.fonttype'] = 42
+
+expName = 'randomSDRVaryingSparsityContinuousLearning'
 plt.figure()
 legendList = []
-epochCheck = [0, 5, 10, 20, 40, 80]
+epochCheck = [0, 5, 10, 20, 40]
 for epoch in epochCheck:
   nrData = np.load \
     ('./results/input_output_overlap/{}/epoch_{}.npz'.format(expName, epoch))
@@ -37,7 +40,7 @@ for epoch in epochCheck:
 plt.legend(legendList)
 plt.xlabel('Noise Level')
 plt.ylabel('Change of SP output')
-plt.savefig('./figures/noise_robustness_{}.pdf'.format(expName))
+plt.savefig('./figures/noise_robustness_{}_beforeChange.pdf'.format(expName))
 
 
 expName = 'randomSDRVaryingSparsityContinuousLearning'