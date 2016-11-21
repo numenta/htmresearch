2c0759d7f9f477d403afec331732c05b01b88d2f
Marion Le Borgne <mleborgne@numenta.com>
2016-11-21 11:03:41 -0800

    added missing copyright headers

--- projects/online_sequence_classification/clustering/plot.py
+++ projects/online_sequence_classification/clustering/plot.py
@@ -1,3 +1,11 @@
+# ----------------------------------------------------------------------
+#  Copyright (C) 2016, Numenta Inc. All rights reserved.
+#
+#  The information and source code contained herein is the
+#  exclusive property of Numenta Inc. No part of this software
+#  may be used, reproduced, stored or distributed in any form,
+#  without explicit written authorization from Numenta Inc.
+# ----------------------------------------------------------------------
 import numpy as np
 from matplotlib import pyplot as plt
 
