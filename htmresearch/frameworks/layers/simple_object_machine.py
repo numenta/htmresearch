900e69d53aab44c446e387ddecfa1aa84a287979
Marcus Lewis <mrcslws@gmail.com>
2016-12-20 17:26:53 -0800

    Bugfix: numpy.random handles its boundaries differently from random

--- htmresearch/frameworks/layers/simple_object_machine.py
+++ htmresearch/frameworks/layers/simple_object_machine.py
@@ -208,7 +208,7 @@ class SimpleObjectMachine(ObjectMachineBase):
       locationArray = numpy.random.permutation(locationArray)
       self.addObject(
         [(locationArray[p],
-          numpy.random.randint(0, numFeatures-1)) for p in xrange(numPoints)],
+          numpy.random.randint(0, numFeatures)) for p in xrange(numPoints)],
       )
 
 
