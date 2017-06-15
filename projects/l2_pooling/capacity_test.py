d0d35e1e2383fb63cade296ef6688d9b8268240b
Max Schwarzer <mschwarzer@Maxs-MacBook-Pro.local>
2017-06-14 17:38:07 -0700

    RES-363 Change capacity test to use more repeats

--- projects/l2_pooling/capacity_test.py
+++ projects/l2_pooling/capacity_test.py
@@ -1460,7 +1460,7 @@ def runExperiment9(resultDirName=DEFAULT_RESULT_DIR_NAME,
   """
 
   numPointsPerObject = 10
-  numRpts = 1
+  numRpts = 3
   objectNumRange = range(10, 1000, 50)
 
   l4Params = getL4Params()
