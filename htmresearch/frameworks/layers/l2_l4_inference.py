d303e62474467af24790453d08e9001d90621529
Max Schwarzer <mschwarzer@Maxs-MacBook-Pro.local>
2017-06-15 13:31:16 -0700

    RES-363 Upload experiment details to domino

--- htmresearch/frameworks/layers/l2_l4_inference.py
+++ htmresearch/frameworks/layers/l2_l4_inference.py
@@ -141,7 +141,7 @@ class L4L2Experiment(object):
                longDistanceConnections = 0,
                maxConnectionDistance = 1,
                columnPositions = None,
-               decayFunction = lambda x: 1./(x**2),
+               decayFunction = lambda x: 1./x,
                L4Overrides=None,
                numLearningPoints=3,
                seed=42,
