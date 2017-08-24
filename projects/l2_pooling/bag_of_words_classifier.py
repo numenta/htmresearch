35d54ab2538e9e6b61cb1bf101ba73edd4319423
Yuwei <ywcui1990@gmail.com>
2017-08-24 16:02:01 -0700

    refactor bow experiments

--- projects/l2_pooling/bag_of_words_classifier.py
+++ projects/l2_pooling/bag_of_words_classifier.py
@@ -33,8 +33,11 @@ python bag_of_words_classifier.py  --location 0
 
 import cPickle
 from optparse import OptionParser
+import os
+import numpy
 import numpy as np
 import  matplotlib.pyplot as plt
+from multiprocessing import Pool, cpu_count
 
 from htmresearch.frameworks.layers.object_machine_factory import (
   createObjectMachine
@@ -91,149 +94,341 @@ def bowClassifierPredict(input, bowVectors, distance="overlap"):
   return output
 
 
-if __name__ == "__main__":
-  (expConfig, _args) = _getArgs()
 
-  # Create the objects
-  numFeatures = 10
-  pointRange = 1
-  numObjects = 50
-  numLocations = 10
-  numPoints = 10
-  numTrials = 10
+def run_bag_of_words_classifier(args):
+  numObjects = args.get("numObjects", 10)
+  numLocations = args.get("numLocations", 10)
+  numFeatures = args.get("numFeatures", 10)
+  numPoints = args.get("numPoints", 10)
+  trialNum = args.get("trialNum", 42)
+  pointRange = args.get("pointRange", 1)
+  useLocation = args.get("useLocation", 1)
+  numColumns = args.get("numColumns", 2)
+  objects = createObjectMachine(
+    machineType="simple",
+    numInputBits=20,
+    sensorInputSize=150,
+    externalInputSize=2400,
+    numCorticalColumns=numColumns,
+    numFeatures=numFeatures,
+    seed=trialNum
+  )
 
-  accuracyList = np.zeros((numTrials, numFeatures))
+  for p in range(pointRange):
+    objects.createRandomObjects(numObjects, numPoints=numPoints + p,
+                                numLocations=numLocations,
+                                numFeatures=numFeatures)
 
-  for trial in range(numTrials):
-    objects = createObjectMachine(
-      machineType="simple",
-      numInputBits=20,
-      sensorInputSize=150,
-      externalInputSize=2400,
-      numCorticalColumns=1,
-      numFeatures=numFeatures,
-      seed=trial
-    )
+  objects = objects.provideObjectsToLearn()
+  objectNames = objects.keys()
+  numObjs = len(objectNames)
+  featureWidth = 150
+  locationWidth = 2400
 
-    for p in range(pointRange):
-      objects.createRandomObjects(numObjects, numPoints=numPoints+p,
-                                        numLocations=numLocations,
-                                        numFeatures=numFeatures)
+  # compute the number of sensations across all objects
+  numInputVectors = 0
+  for i in range(numObjs):
+    numInputVectors += len(objects[objectNames[i]])
 
-    objects = objects.provideObjectsToLearn()
-    objectNames = objects.keys()
-    numObjs = len(objectNames)
-    featureWidth = 150
-    locationWidth = 2400
-    useLocation = expConfig.useLocation
+  if useLocation:
+    inputWidth = featureWidth + locationWidth
+  else:
+    inputWidth = featureWidth
 
-
-    # compute the number of sensations across all objects
-    numInputVectors = 0
-    for i in range(numObjs):
-      numInputVectors += len(objects[objectNames[i]])
-
-    if useLocation:
-      inputWidth = featureWidth + locationWidth
-    else:
-      inputWidth = featureWidth
-
-    # create "training" dataset
-    data = np.zeros((numInputVectors, inputWidth))
-    label = np.zeros((numInputVectors, numObjs))
-    k = 0
-    for i in range(numObjs):
-      print "converting object {} ...".format(i)
-      numSenses = len( objects[objectNames[i]])
-      for j in range(numSenses):
+  # create "training" dataset
+  data = np.zeros((numInputVectors, inputWidth))
+  label = np.zeros((numInputVectors, numObjs))
+  k = 0
+  for i in range(numObjs):
+    # print "converting object {} ...".format(i)
+    numSenses = len(objects[objectNames[i]])
+    for j in range(numSenses):
+      activeBitsFeature = np.array(list(objects[objectNames[i]][j][0][1]))
+      data[k, activeBitsFeature] = 1
+      if useLocation:
         activeBitsLocation = np.array(list(objects[objectNames[i]][j][0][0]))
-        activeBitsFeature = np.array(list(objects[objectNames[i]][j][0][1]))
+        data[k, featureWidth + activeBitsLocation] = 1
+      label[k, i] = 1
+      k += 1
 
-        data[k, activeBitsFeature] = 1
-        if useLocation:
-          data[k, featureWidth+activeBitsLocation] = 1
-        label[k, i] = 1
-        k += 1
+  # enumerate number of distinct "words"
+  wordList = np.zeros((0, inputWidth), dtype='int32')
+  featureList = np.zeros((data.shape[0],))
+  for i in range(data.shape[0]):
+    findWord = False
+    for j in range(wordList.shape[0]):
+      index = findWordInVocabulary(data[i, :], wordList)
+      if index is not None:
+        featureList[i] = index
+        findWord = True
+        break
 
-    # enumerate number of distinct "words"
-    wordList = np.zeros((0, inputWidth), dtype='int32')
-    featureList = np.zeros((data.shape[0], ))
-    for i in range(data.shape[0]):
-      findWord = False
-      for j in range(wordList.shape[0]):
-        index = findWordInVocabulary(data[i, :], wordList)
-        if index is not None:
-          featureList[i] = index
-          findWord = True
-          break
+    if findWord is False:
+      newWord = np.zeros((1, inputWidth), dtype='int32')
+      newWord[0, :] = data[i, :]
+      wordList = np.concatenate((wordList, newWord))
+      featureList[i] = wordList.shape[0] - 1
 
-      if findWord is False:
-        newWord = np.zeros((1, inputWidth), dtype='int32')
-        newWord[0, :] = data[i, :]
-        wordList = np.concatenate((wordList, newWord))
-        featureList[i] = wordList.shape[0]-1
+  numWords = wordList.shape[0]
+  # wordList = wordList[np.random.permutation(np.arange(numWords)), :]
+  print "object # {} feature #  {} location # {} distinct words # {} numColumns {}".format(
+    numObjects, numFeatures, numLocations, numWords, numColumns)
 
-    numWords = wordList.shape[0]
-    wordList = wordList[np.random.permutation(np.arange(numWords)), :]
-    print "number of distinct words {}".format(numWords)
+  # convert objects to BOW representations
+  bowVectors = np.zeros((numObjs, numWords))
+  k = 0
+  for i in range(numObjs):
+    numSenses = len(objects[objectNames[i]])
+    for j in range(numSenses):
+      index = findWordInVocabulary(data[k, :], wordList)
+      bowVectors[i, index] += 1
+      k += 1
 
-    # convert objects to BOW representations
-    bowVectors = np.zeros((numObjs, numWords))
-    k = 0
+  # plt.figure()
+  # plt.imshow(np.transpose(bowVectors))
+  # plt.xlabel('Object #')
+  # plt.ylabel('Word #')
+  # plt.title("BoW representations")
+
+
+  # plot accuracy as a function of number of sensations
+  accuracyList = []
+  classificationOutcome = np.zeros((numObjs, 11))
+  for maxSenses in range(1, 11):
+    bowVectorsTest = np.zeros((numObjs, numWords))
+    offset = 0
     for i in range(numObjs):
-      numSenses = len(objects[objectNames[i]])
-      for j in range(numSenses):
-        index = findWordInVocabulary(data[k, :], wordList)
-        bowVectors[i, index] += 1
-        k += 1
+      numSenses = min(len(objects[objectNames[i]]), maxSenses)
 
-    plt.figure()
-    plt.imshow(np.transpose(bowVectors))
-    plt.xlabel('Object #')
-    plt.ylabel('Word #')
-    plt.title("BoW representations")
+      # sensations for object i
+      senses = np.where(bowVectors[i, :])[0]
 
-
-    numCorrect = 0
-    for i in range(numObjs):
-      output = bowClassifierPredict(bowVectors[i, :], bowVectors)
-      predictLabel = np.argmin(output)
-      numCorrect += predictLabel == i
-      print " true label {} predicted label {}".format(i, predictLabel)
-    print "BOW classifier accuracy {}".format(float(numCorrect)/numObjs)
-
-    # plot accuracy as a function of number of sensations
-    for maxSenses in range(1, 11):
-      bowVectorsTest = np.zeros((numObjs, numWords))
-      offset = 0
-      for i in range(numObjs):
-        numSenses = len(objects[objectNames[i]])
-        numSenses = min(numSenses, maxSenses)
+      for c in range(numColumns):
         for j in range(numSenses):
-          index = findWordInVocabulary(data[offset+j, :], wordList)
+          index = np.random.choice(senses)
+          # index = findWordInVocabulary(data[offset + j, :], wordList)
           bowVectorsTest[i, index] += 1
         offset += len(objects[objectNames[i]])
 
-      numCorrect = 0
-      for i in range(numObjs):
-        output = bowClassifierPredict(bowVectorsTest[i, :], bowVectors)
-        predictLabel = np.argmin(output)
-        numCorrect += predictLabel == i
-      accuracy = float(numCorrect)/numObjs
-      accuracyList[trial, maxSenses-1] = accuracy
-      print "maxSenses {} accuracy {}".format(maxSenses, accuracy)
+    numCorrect = 0
+    for i in range(numObjs):
+      output = bowClassifierPredict(bowVectorsTest[i, :], bowVectors)
+      predictLabel = np.argmin(output)
+      outcome = predictLabel == i
+      numCorrect += outcome
+      classificationOutcome[i, maxSenses] = outcome
+    accuracy = float(numCorrect) / numObjs
+    accuracyList.append(accuracy)
+    # print "maxSenses {} accuracy {}".format(maxSenses, accuracy)
 
-  result = {"numTouches": range(1, 11),
-            "accuracy": accuracyList}
+  convergencePoint = np.zeros((numObjects, ))
+  for i in range(numObjects):
+    convergencePoint[i] = np.where(classificationOutcome[i, :] == 1)[0][0]
 
-  # Pickle results for later use
+  args.update({"accuracy": accuracyList})
+  args.update({"numTouches": range(1, 11)})
+  args.update({"convergencePoint": np.mean(convergencePoint)})
+  args.update({"classificationOutcome": classificationOutcome})
+
+  return args
+
+
+def plotConvergenceByObject(results, objectRange, featureRange, numTrials,
+                            linestyle='-'):
+  """
+  Plots the convergence graph: iterations vs number of objects.
+  Each curve shows the convergence for a given number of unique features.
+  """
+  ########################################################################
+  #
+  # Accumulate all the results per column in a convergence array.
+  #
+  # Convergence[f,o] = how long it took it to converge with f unique features
+  # and o objects.
+
+  convergence = numpy.zeros((max(featureRange), max(objectRange) + 1))
+  for r in results:
+    if r["numFeatures"] in featureRange:
+      convergence[r["numFeatures"] - 1, r["numObjects"]] += r["convergencePoint"]
+
+  convergence /= numTrials
+
+  ########################################################################
+  #
+  # Create the plot. x-axis=
+
+  # Plot each curve
+  legendList = []
+  colorList = ['r', 'b', 'g', 'm', 'c', 'k', 'y']
+
+  for i in range(len(featureRange)):
+    f = featureRange[i]
+    print "features={} objectRange={} convergence={}".format(
+      f,objectRange, convergence[f-1,objectRange])
+    legendList.append('Unique features={}'.format(f))
+    plt.plot(objectRange, convergence[f-1, objectRange],
+             color=colorList[i], linestyle=linestyle)
+
+  # format
+  plt.legend(legendList, loc="lower right", prop={'size':10})
+  plt.xlabel("Number of objects in training set")
+  plt.xticks(range(0,max(objectRange)+1,10))
+  plt.yticks(range(0,int(convergence.max())+2))
+  plt.ylabel("Average number of touches")
+  plt.title("Number of touches to recognize one object (single column)")
+
+
+
+
+def plotConvergenceByColumn(results, columnRange, featureRange, numTrials):
+  """
+  Plots the convergence graph: iterations vs number of columns.
+  Each curve shows the convergence for a given number of unique features.
+  """
+  ########################################################################
+  #
+  # Accumulate all the results per column in a convergence array.
+  #
+  # Convergence[f,c] = how long it took it to  converge with f unique features
+  # and c columns.
+  convergence = numpy.zeros((max(featureRange), max(columnRange) + 1))
+  for r in results:
+    convergence[r["numFeatures"] - 1,
+                r["numColumns"]] += r["convergencePoint"]
+  convergence /= numTrials
+  # For each column, print convergence as fct of number of unique features
+  for c in range(1, max(columnRange) + 1):
+    print c, convergence[:, c]
+  # Print everything anyway for debugging
+  print "Average convergence array=", convergence
+  ########################################################################
+  #
+  # Create the plot. x-axis=
+  plt.figure()
+  plotPath = os.path.join("plots", "bow_convergence_by_column.pdf")
+  # Plot each curve
+  legendList = []
+  colorList = ['r', 'b', 'g', 'm', 'c', 'k', 'y']
+  for i in range(len(featureRange)):
+    f = featureRange[i]
+    print columnRange
+    print convergence[f-1,columnRange]
+    legendList.append('Unique features={}'.format(f))
+    plt.plot(columnRange, convergence[f-1,columnRange],
+             color=colorList[i])
+  # format
+  plt.legend(legendList, loc="upper right")
+  plt.xlabel("Number of columns")
+  plt.xticks(columnRange)
+  plt.ylim([0, 4])
+  plt.yticks(range(0,int(convergence.max())+1))
+  plt.ylabel("Average number of touches")
+  plt.title("Number of touches to recognize one object (multiple columns)")
+    # save
+  plt.savefig(plotPath)
+  # plt.close()
+
+
+def run_bow_experiment_single_column():
+  # Create the objects
+  featureRange = [5, 10, 20, 30]
+  pointRange = 1
+  objectRange = [2, 10, 20, 30, 40, 50, 60, 80, 100]
+  numLocations = [10]
+  numPoints = 10
+  numTrials = 10
+  numColumns = [1]
+  useLocation = 1
+  args = []
+  for c in reversed(numColumns):
+    for o in reversed(objectRange):
+      for l in numLocations:
+        for f in featureRange:
+          for t in range(numTrials):
+            args.append(
+              {"numObjects": o,
+               "numLocations": l,
+               "numFeatures": f,
+               "numColumns": c,
+               "trialNum": t,
+               "pointRange": pointRange,
+               "numPoints": numPoints,
+               "useLocation": useLocation
+               }
+
+          )
+
+  # numWorkers = cpu_count()
+  # pool = Pool(processes=numWorkers)
+  # result = pool.map(run_bag_of_words_classifier, args)
+
   resultsName = "bag_of_words_useLocation_{}.pkl".format(useLocation)
-  with open(resultsName,"wb") as f:
-    cPickle.dump(result,f)
+  # # Pickle results for later use
+  # with open(resultsName, "wb") as f:
+  #   cPickle.dump(result, f)
+
 
   plt.figure()
-  plt.plot(range(10), np.mean(accuracyList, 0))
-  plt.xlabel('number of sensations')
-  plt.ylabel('accuracy ')
+  # with open("object_convergence_results.pkl", "rb") as f:
+  #   results = cPickle.load(f)
+  # plotConvergenceByObject(results, objectRange, featureRange, linestyle='-')
 
+  with open(resultsName, "rb") as f:
+    resultsBOW = cPickle.load(f)
+  plotConvergenceByObject(resultsBOW, objectRange, featureRange, numTrials,
+                          linestyle='--')
+  plotPath = os.path.join("plots",
+                          "convergence_by_object_random_location_bow.pdf")
+  plt.savefig(plotPath)
+
+
+def run_bow_experiment_multiple_columns():
+  # Create the objects
+  featureRange = [5, 10, 20, 30]
+  pointRange = 1
+  objectRange = [100]
+  numLocations = [10]
+  numPoints = 10
+  numTrials = 10
+  columnRange = [1, 2, 3, 4, 5, 6, 7, 8]
+  useLocation = 1
+  args = []
+  for c in reversed(columnRange):
+    for o in reversed(objectRange):
+      for l in numLocations:
+        for f in featureRange:
+          for t in range(numTrials):
+            args.append(
+              {"numObjects": o,
+               "numLocations": l,
+               "numFeatures": f,
+               "numColumns": c,
+               "trialNum": t,
+               "pointRange": pointRange,
+               "numPoints": numPoints,
+               "useLocation": useLocation
+               }
+            )
+
+  numWorkers = cpu_count()
+  # pool = Pool(processes=numWorkers)
+  # result = pool.map(run_bag_of_words_classifier, args)
+
+  resultsName = "bag_of_words_multi_column_useLocation_{}.pkl".format(useLocation)
+  # # Pickle results for later use
+  # with open(resultsName, "wb") as f:
+  #   cPickle.dump(result, f)
+
+  with open(resultsName, "rb") as f:
+    resultsBOW = cPickle.load(f)
+
+  plt.figure()
+  plotConvergenceByColumn(resultsBOW, columnRange, featureRange, numTrials)
+
+
+if __name__ == "__main__":
+  (expConfig, _args) = _getArgs()
+  # run_bow_experiment_single_column()
+
+  run_bow_experiment_multiple_columns()
 
