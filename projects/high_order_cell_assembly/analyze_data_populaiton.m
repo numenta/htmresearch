867337ef7d3282b38ed216f527018c7f321fc69d
Yuwei <ywcui1990@gmail.com>
2017-09-08 14:40:18 -0700

    add license statement

--- projects/high_order_cell_assembly/analyze_data_populaiton.m
+++ projects/high_order_cell_assembly/analyze_data_populaiton.m
@@ -1,3 +1,25 @@
+%!/usr/bin/env python
+% ----------------------------------------------------------------------
+% Numenta Platform for Intelligent Computing (NuPIC)
+% Copyright (C) 2016, Numenta, Inc.  Unless you have an agreement
+% with Numenta, Inc., for a separate license for this software code, the
+% following terms and conditions apply:
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU Affero Public License version 3 as
+% published by the Free Software Foundation.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+% See the GNU Affero Public License for more details.
+%
+% You should have received a copy of the GNU Affero Public License
+% along with this program.  If not, see http://www.gnu.org/licenses.
+%
+% http://numenta.org/licenses/
+% ----------------------------------------------------------------------
+
 clear all;
 close all;
 addpath('./src/');
@@ -93,7 +115,7 @@ for exp = 1%:length(DataFolderList)
     % xl = [4.6 5.6];
     smoothK = ones(4, 1)/4;
     figure(4);clf;
-%     subplot(4,2,1);
+    subplot(4,2,1);
     colors = {'r', 'g', 'b'};
     psthSingleNeuron = zeros(numFramesPerStim, 3);
     for i = 1:3
