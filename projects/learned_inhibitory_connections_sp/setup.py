f6852566fc469b921e3db416b3f2119c65be408b
Mirko Klukas <mklukas@Mirkos-MacBook-Pro.local>
2017-12-08 13:56:27 -0800

    changed folder structure

--- projects/learned_inhibitory_connections_sp/setup.py
+++ projects/learned_inhibitory_connections_sp/setup.py
@@ -1,9 +1,11 @@
-from distutils.core import setup
+
+from setuptools import setup, find_packages
 
 setup(name='LateralPooler',
       version='1.0',
       description='Experimental spatial pooler implementation',
       author='Mirko Klukas',
       author_email='mklukas@numenta.com',
-      packages=['lateralpooler'],
+      package_dir={"": "src"},
+      packages=find_packages("src")
      )
