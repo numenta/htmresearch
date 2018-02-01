212c42e2aa12f9739c9ee227df91b446219b0e1d
Luiz Scheinkman <lscheinkman@numenta.com>
2018-01-31 16:16:39 -0800

    Fix matlab port to python

--- projects/high_order_cell_assembly/calculate_sparsity_over_trials.py
+++ projects/high_order_cell_assembly/calculate_sparsity_over_trials.py
@@ -1,34 +1,32 @@
-# % Numenta platform for Intelligent Computing (NuPIC)
-# % Copyright (C) 2016, Numenta, Inc.  Unless you have an agreement
-# % with Numenta, Inc., for a separate license for this software code, the
-# % following terms and conditions apply:
-# %
-# % This program is free software: you can redistribute it and/or modify
-# % it under the terms of the GNU Affero Public License version 3 as
-# % published by the Free Software Foundation.
-# %
-# % This program is distributed in the hope that it will be useful,
-# % but WITHOUT ANY WARRANTY; without even the implied warranty of
-# % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# % See the GNU Affero Public License for more details.
-# %
-# % You should have received a copy of the GNU Affero Public License
-# % along with this program.  If not, see http://www.gnu.org/licenses.
-# %
-# % http://numenta.org/licenses/
-# % ----------------------------------------------------------------------
+#!/usr/bin/env python
+# Numenta Platform for Intelligent Computing (NuPIC)
+# Copyright (C) 2018, Numenta, Inc.  Unless you have an agreement
+# with Numenta, Inc., for a separate license for this software code, the
+# following terms and conditions apply:
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero Public License for more details.
+#
+# You should have received a copy of the GNU Affero Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# http://numenta.org/licenses/
+# ----------------------------------------------------------------------
+
 import numpy as np
 
 def calculate_sparsity_over_trials(spikeMat, imgPara):
-    print spikeMat.shape
-    stimrep = int(imgPara['stimrep'])
-    sparsity = np.zeros((stimrep,1))
-    numFramesPerStim = int(round(imgPara['stim_time']/imgPara['dt']))
-    # the returned sparsity is everaged for all the neurons' 
+    sparsity = np.zeros((imgPara.stimrep, 1))
+    # the returned sparsity is everaged for all the neurons'
     # firings within each specific trial
-    for rep in range(stimrep):
-        numFramesArray = np.array(range(numFramesPerStim))
-        spikesCurrentTrial = spikeMat[rep]  
-        sparsity[rep] = np.mean(spikesCurrentTrial) # average by columns and by rows    
+    for rep in xrange(imgPara.stimrep):
+        spikesCurrentTrial = spikeMat[rep]
+        sparsity[rep] = np.mean(spikesCurrentTrial)  # average by columns and by rows
     return sparsity
 
--- projects/high_order_cell_assembly/get_response_mat.py
+++ projects/high_order_cell_assembly/get_response_mat.py
@@ -1,35 +1,50 @@
+#!/usr/bin/env python
+# Numenta Platform for Intelligent Computing (NuPIC)
+# Copyright (C) 2018, Numenta, Inc.  Unless you have an agreement
+# with Numenta, Inc., for a separate license for this software code, the
+# following terms and conditions apply:
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero Public License for more details.
+#
+# You should have received a copy of the GNU Affero Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# http://numenta.org/licenses/
+# ----------------------------------------------------------------------
+import numpy as np
+
 def get_response_mat(spiketrain, imgPara, stimType, goodCells, plotRaster):
     
-    numNeuron = len(goodCells) # the number of neurons
-    print numNeuron
-    
+    numNeuron = len(goodCells)  # the number of neurons
     # imgPara.stim_time = 32s, imgPara.dt = 0.075103, 
     # numFramesPerStim is the number of the frames within 32s movie stimulus
-    
-    numFramesPerStim = int(round(imgPara['stim_time'] / imgPara['dt']))
-    # print numFramesPerStim
+    numFramesPerStim = int(round(imgPara.stim_time / imgPara.dt))
+
     spikeMat = []
-    ## generate the spike timing for all the neurons through all trials
-    for rep in range(imgPara['stimrep']):    
+    # generate the spike timing for all the neurons through all trials
+    for rep in xrange(imgPara.stimrep):
         spikesCurrentTrial = np.zeros((numNeuron, numFramesPerStim))
         spikesRaster = []
         cellI = 0
-        for i in range(len(goodCells)):
+        for i in goodCells:
             # spikesI: spiking timing of a specific neuron at a specific trial
-            # print i
-            spikesI = spiketrain[0,i][0][rep,stimType]
-            # print spikesI
-            spikesI = np.round(spikesI[np.nonzero(spikesI<=numFramesPerStim)])
-            #print spikesI
-            spikesI = spikesI[np.nonzero(spikesI>0)];
+            spikesI = np.atleast_1d(spiketrain[i].st[rep, stimType - 1])
+            spikesI = np.round(spikesI[np.nonzero(spikesI <= numFramesPerStim)])
+            spikesI = spikesI[np.nonzero(spikesI > 0)]
             spikesI = spikesI.astype(int)
-            
             spikesI = spikesI - 1
-            # print spikesI
+
             # along the 426 frames, spike timings was labeled
-            spikesCurrentTrial[cellI,spikesI] = 1
-            cellI  = cellI +1;
-            spikesRaster.append(spikesI*imgPara['dt'] -1)
+            spikesCurrentTrial[cellI, spikesI] = 1
+            cellI = cellI + 1
+            spikesRaster.append(spikesI * imgPara.dt - 1)
         
 
         # return spikeMat as the spiking time for all neurons
@@ -37,6 +52,4 @@ def get_response_mat(spiketrain, imgPara, stimType, goodCells, plotRaster):
     
     # change spikeMat to be numpy array
     spikeMat = np.array(spikeMat)   
-    print spikeMat.shape
     return spikeMat
-            
