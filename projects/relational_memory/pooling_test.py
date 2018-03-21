6e08edfe7b92b9c70cce0b28caba80743b669efe
Scott Purdy <spurdy@numenta.com>
2018-03-21 15:59:53 -0700

    Finish work adding pooler and pooler test showing that it doesn't work as currently implemented

--- projects/relational_memory/pooling_test.py
+++ projects/relational_memory/pooling_test.py
@@ -21,41 +21,76 @@
 
 """TODO"""
 
+import collections
+
 import numpy as np
 
 from htmresearch.algorithms.column_pooler import ColumnPooler
 from nupic.algorithms.knn_classifier import KNNClassifier
 
+TRAIN_FEATURES = dict([(i, collections.defaultdict(int)) for i in xrange(2)])
+TEST_FEATURES = dict([(i, collections.defaultdict(int)) for i in xrange(2)])
 
-def train(pooler, classifier, objs):
+
+def train(pooler, classifier, objs, numPasses):
+  for label, obj in objs:
+    pooler.reset()
+
+    for _ in xrange(numPasses):
+      np.random.shuffle(obj)
+      for feature in obj:
+        sortedFeature = sorted(set(feature))
+        for v in sortedFeature:
+          TRAIN_FEATURES[label][v] += 1
+        pooler.compute(feedforwardInput=sortedFeature,
+                       learn=True,
+                       predictedInput=sortedFeature)
+        poolerOutput = pooler.getActiveCells()
+        classifierInput = np.zeros(4096, dtype=np.uint32)
+        classifierInput[poolerOutput] = 1
+        classifier.learn(classifierInput, label)
+
+
+def test(pooler, classifier, objs):
+  total = 0
+  correct = 0
   for label, obj in objs:
     pooler.reset()
 
     np.random.shuffle(obj)
+    classifierGuesses = collections.defaultdict(int)
     for feature in obj:
-      sortedFeature = np.sort(feature)
-      print "A"
+      sortedFeature = sorted(set(feature))
+      for v in sortedFeature:
+        TEST_FEATURES[label][v] += 1
       poolerOutput = pooler.compute(feedforwardInput=sortedFeature,
-                                    learn=True,
+                                    learn=False,
                                     predictedInput=sortedFeature)
-      print "B"
-      classifierInput = np.zeros(4096, dtype=np.int32)
+
+      classifierInput = np.zeros(4096, dtype=np.uint32)
       classifierInput[poolerOutput] = 1
-      classifier.learn(classifierInput, label)
+      classifierResult = classifier.infer(classifierInput)
 
+      classifierGuesses[classifierResult[0]] += 1
 
-def test(pooler, classifier, objs):
-  pass
+    #bestGuess = sorted(classifierGuesses.iteritems(), key=lambda x: x[1])[-1][0]
+    bestGuess = classifierResult[0]
+    if bestGuess == label:
+      correct += 1
+    total += 1
+  return float(correct) / float(total)
 
 
 def run():
-  numObjects = 100
-  objSize = 10
-  allIndices = np.array(xrange(1024), dtype=np.int32)
+  numObjects = 2
+  objSize = 4
+  numPasses = 2
+
+  allIndices = np.array(xrange(1024), dtype=np.uint32)
   objs = [
       (
           label,
-          [np.random.choice(allIndices, 20) for _ in xrange(objSize)]
+          [[int(i) for i in np.random.choice(allIndices, 20)] for _ in xrange(objSize)]
       )
       for label in xrange(numObjects)
   ]
@@ -65,9 +100,10 @@ def run():
   )
   classifier = KNNClassifier(k=1, distanceMethod="rawOverlap")
 
-  train(pooler, classifier, objs)
+  train(pooler, classifier, objs, numPasses)
 
-  test(pooler, classifier, objs)
+  result = test(pooler, classifier, objs)
+  print result
 
 
 if __name__ == "__main__":
