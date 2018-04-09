4d0821d295f9228bd7bca6b5b7ca5f652c20801d
Subutai Ahmad <sahmad@numenta.com>
2018-04-09 09:56:05 -0700

    Support for multiprocessing and Domino
    Some small speedups

--- projects/sdr_paper/poirazi_neuron_model/run_dim_classification_experiment.py
+++ projects/sdr_paper/poirazi_neuron_model/run_dim_classification_experiment.py
@@ -20,55 +20,100 @@
 # ----------------------------------------------------------------------
 
 import numpy
-import random
-from htmresearch.frameworks.poirazi_neuron_model.neuron_model import (
-  power_nonlinearity, threshold_nonlinearity, sigmoid_nonlinearity)
+import time
+import os
+from multiprocessing import Pool, cpu_count
+import cPickle
+
+from htmresearch.frameworks.poirazi_neuron_model.neuron_model import sigmoid_nonlinearity
 from htmresearch.frameworks.poirazi_neuron_model.neuron_model import Matrix_Neuron as Neuron
 from htmresearch.frameworks.poirazi_neuron_model.data_tools import generate_evenly_distributed_data_sparse
-numpy.random.seed(19)
 
-def run_false_positive_experiment_dim(num_neurons = 1,
-                    num_neg_neurons = 1,
-                    a = 128,
-                    test_dims = range(1100, 2100, 200),
-                    num_samples = 1000,
-                    num_dendrites = 500,
-                    dendrite_length = 24,
-                    num_trials = 10000,
-                    nonlinearity = sigmoid_nonlinearity(11.5, 5)):
+def run_false_positive_experiment_dim(
+                              numActive = 128,
+                              dim = 500,
+                              numSamples = 1000,
+                              numDendrites = 500,
+                              synapses = 24,
+                              numTrials = 10000,
+                              seed = 42,
+                              nonlinearity = sigmoid_nonlinearity(11.5, 5)):
   """
-  Run an experiment to test the false positive rate based on number of
-  synapses per dendrite, dimension and sparsity.  Uses two competing neurons,
-  along the P&M model.
+  Run an experiment to test the false positive rate based on number of synapses
+  per dendrite, dimension and sparsity.  Uses two competing neurons, along the
+  P&M model.
 
   Based on figure 5B in the original SDR paper.
   """
-  for dim in test_dims:
+  numpy.random.seed(seed)
+  fps = []
+  fns = []
+  totalUnclassified = 0
 
-    fps = []
-    fns = []
+  for trial in range(numTrials):
 
-    for trial in range(num_trials):
+    # data = generate_evenly_distributed_data_sparse(dim = dim,
+    #                                                num_active = numActive,
+    #                                                num_samples = numSamples)
+    # labels = numpy.asarray([1 for i in range(numSamples / 2)] +
+    #                        [-1 for i in range(numSamples / 2)])
+    # flipped_labels = labels * -1
 
-      neuron = Neuron(size = dendrite_length*num_dendrites, num_dendrites = num_dendrites, dendrite_length = dendrite_length, dim = dim, nonlinearity = nonlinearity)
-      neg_neuron = Neuron(size = dendrite_length*num_dendrites, num_dendrites = num_dendrites, dendrite_length = dendrite_length, dim = dim, nonlinearity = nonlinearity)
-      data = generate_evenly_distributed_data_sparse(dim = dim, num_active = a, num_samples = num_samples)
-      labels = numpy.asarray([1 for i in range(num_samples/2)] + [-1 for i in range(num_samples/2)])
-      flipped_labels = labels * -1
+    negData = generate_evenly_distributed_data_sparse(dim = dim,
+                                                   num_active = numActive,
+                                                   num_samples = numSamples/2)
+    posData = generate_evenly_distributed_data_sparse(dim = dim,
+                                                   num_active = numActive,
+                                                   num_samples = numSamples/2)
+    halfLabels = numpy.asarray([1 for _ in range(numSamples / 2)])
+    flippedHalfLabels = halfLabels * -1
 
-      neuron.HTM_style_initialize_on_data(data, labels)
-      neg_neuron.HTM_style_initialize_on_data(data, flipped_labels)
+    neuron = Neuron(size =synapses * numDendrites,
+                    num_dendrites = numDendrites,
+                    dendrite_length = synapses,
+                    dim = dim, nonlinearity = nonlinearity)
+    neg_neuron = Neuron(size =synapses * numDendrites,
+                        num_dendrites = numDendrites,
+                        dendrite_length = synapses,
+                        dim = dim, nonlinearity = nonlinearity)
 
-      error, fp, fn, uc = get_error(data, labels, [neuron], [neg_neuron], add_noise = True)
+    neuron.HTM_style_initialize_on_positive_data(posData)
+    neg_neuron.HTM_style_initialize_on_positive_data(negData)
 
-      fps.append(fp)
-      fns.append(fn)
-      print "Error at n = {} is {}, with {} false positives and {} false negatives, with {} unclassified".format(dim, error, fp, fn, uc)
+    # Get error for positively labeled data
+    fp, fn, uc = get_error(posData, halfLabels, [neuron], [neg_neuron])
+    totalUnclassified += uc
+    fps.append(fp)
+    fns.append(fn)
 
-    with open("pm_dim_FP_{}.txt".format(a), "a") as f:
-      f.write(str(dim) + ", " + str(sum(fns + fps)) + ", " + str(num_trials*num_samples) + "\n")
+    # Get error for negatively labeled data
+    fp, fn, uc = get_error(negData, flippedHalfLabels, [neuron], [neg_neuron])
+    totalUnclassified += uc
+    fps.append(fp)
+    fns.append(fn)
 
-def get_error(data, labels, pos_neurons, neg_neurons = [], add_noise = False):
+
+  print "Error with n = {} : {} FP, {} FN, {} unclassified".format(
+    dim, sum(fps), sum(fns), totalUnclassified)
+
+  result = {
+    "dim": dim,
+    "totalFP": sum(fps),
+    "totalFN": sum(fns),
+    "total mistakes": sum(fns + fps) + totalUnclassified,
+    "error": float(sum(fns + fps) + totalUnclassified) / (numTrials * numSamples),
+    "totalSamples": numTrials * numSamples,
+    "a": numActive,
+    "num_dendrites": numDendrites,
+    "totalUnclassified": totalUnclassified,
+    "synapses": 24,
+    "seed": seed,
+  }
+
+  return result
+
+
+def get_error(data, labels, pos_neurons, neg_neurons = [], add_noise = True):
   """
   Calculates error, including number of false positives and false negatives.
 
@@ -100,8 +145,73 @@ def get_error(data, labels, pos_neurons, neg_neurons = [], add_noise = False):
       num_false_positives += 1
     elif classification < 0 and label >= 1:
       num_false_negatives += 1
-  return (1.*num_false_positives + num_false_negatives + num_unclassified)/data.nRows(), num_false_positives, num_false_negatives, num_unclassified
+  return num_false_positives, num_false_negatives, num_unclassified
+
+
+def exp_wrapper(params):
+  return run_false_positive_experiment_dim(**params)
+
+
+def runExperiments(resultsFilename):
+  exp_params = []
+  for dim in reversed(range(400, 1600, 200)):
+    exp_params.append({
+      "dim": dim,
+      "numTrials" : 1000,
+      "seed" : dim,
+      "numActive": 128,
+    })
+  for dim in reversed(range(400, 3000, 200)):
+    exp_params.append({
+      "dim": dim,
+      "numTrials" : 1000,
+      "seed" : dim,
+      "numActive": 256,
+    })
+  for dim in reversed(range(400, 1000, 200)):
+    exp_params.append({
+      "dim": dim,
+      "numTrials" : 1000,
+      "seed" : dim,
+      "numActive": 64,
+    })
+  numExperiments = len(exp_params)
+
+  pool = Pool(processes=cpu_count()-1)
+  rs = pool.map_async(exp_wrapper, exp_params, chunksize=1)
+  while not rs.ready():
+    remaining = rs._number_left
+    pctDone = 100.0 - (100.0*remaining) / numExperiments
+    print "    =>", remaining, "experiments remaining, percent complete=",pctDone
+    time.sleep(5)
+  pool.close()  # No more work
+  pool.join()
+  result = rs.get()
+
+  # Pickle results for later use
+  with open(resultsFilename,"wb") as f:
+    cPickle.dump(result,f)
+
+
+def printResults(results):
+  headers = ["a", "dim", "error", "dendrite_length"]
+  for h in headers: print h,
+  print
+
+  for i, r in enumerate(results):
+    for h in headers: print r[h],
+    print
 
 
 if __name__ == "__main__":
-  run_false_positive_experiment_dim()
+
+  # print run_false_positive_experiment_dim(dim=800, numTrials=10)
+  dirName = os.path.dirname(os.path.realpath(__file__))
+  resultsFilename = os.path.join(dirName, "classificationResults.pkl")
+
+  # runExperiments(resultsFilename)
+
+  # Debugging
+  with open(resultsFilename, "rb") as f:
+    results = cPickle.load(f)
+    printResults(results)
