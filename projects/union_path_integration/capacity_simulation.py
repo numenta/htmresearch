Bugfix: Specify orientation in radians, not degrees

projects/union_path_integration/capacity_simulation.py
@@ -157,7 +157,7 @@ def doExperiment(locationModuleWidth,
     locationConfigs.append({
       "cellsPerAxis": locationModuleWidth,
       "scale": scale,
-      "orientation": orientation,
+      "orientation": np.radians(orientation),
       "cellCoordinateOffsets": cellCoordinateOffsets,
       "activationThreshold": 8,
       "initialPermanence": 1.0,

projects/union_path_integration/gaussian_simulation.py
@@ -109,7 +109,7 @@ def doExperiment(numObjects,
 
     locationConfigs.append({
         "scale": scale,
-        "orientation": orientation,
+        "orientation": np.radians(orientation),
         "activationThreshold": 8,
         "initialPermanence": 1.0,
         "connectedPermanence": 0.5,