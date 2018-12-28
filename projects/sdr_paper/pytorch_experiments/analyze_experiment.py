ac7cd349e35a95ccde48726b57deb1292d8301c2
Subutai Ahmad <sahmad@numenta.com>
2018-12-28 14:40:39 -0800

    Print learning curve

--- projects/sdr_paper/pytorch_experiments/analyze_experiment.py
+++ projects/sdr_paper/pytorch_experiments/analyze_experiment.py
@@ -59,7 +59,11 @@ def analyzeParameters(expName, suite):
     print("Couldn't load experiment",expName)
 
 
-def analyzeResults(expName, suite):
+def summarizeResults(expName, suite):
+  """
+  Summarize the totalCorrect value from the last iteration for each experiment
+  in the directory tree.
+  """
   print("\n================",expName,"=====================")
 
   try:
@@ -77,11 +81,13 @@ def analyzeResults(expName, suite):
     print("Couldn't analyze experiment",expName)
 
 
-def printExperimentSpecifics(expPath, suite):
-  # List the noise values from the best one
+def lastNoiseCurve(expPath, suite):
+  """
+  Print the noise errors from the last iteration of this experiment
+  """
   noiseValues = ["0.0", "0.05", "0.1", "0.15", "0.2", "0.25", "0.3",
                             "0.35", "0.4", "0.45", "0.5"]
-  print("\n================",expPath,"=====================")
+  print("\nNOISE CURVE ================",expPath,"=====================")
   try:
     result = suite.get_value(expPath, 0, noiseValues, "last")
     for k in noiseValues:
@@ -92,7 +98,21 @@ def printExperimentSpecifics(expPath, suite):
     print("Couldn't load experiment",expPath)
 
 
-# Need to run it from htmresearch top level:
+def learningCurve(expPath, suite):
+  """
+  Print the test and overall noise errors from each iteration of this experiment
+  """
+  print("\nLEARNING CURVE ================",expPath,"=====================")
+  try:
+    result = suite.get_value(expPath, 0, ["testerror","totalCorrect","elapsedTime"], "all")
+    print("i testerror totalCorrect elapsedTime")
+    for i,v in enumerate(zip(result["testerror"],result["totalCorrect"],result["elapsedTime"])):
+      print(i,v[0],v[1],int(v[2]))
+  except:
+    print("Couldn't load experiment",expPath)
+
+
+# To run it from htmresearch top level:
 # python projects/sdr_paper/pytorch_experiments/analyze_experiment.py -c projects/sdr_paper/pytorch_experiments/experiments.cfg
 
 if __name__ == '__main__':
@@ -107,28 +127,11 @@ if __name__ == '__main__':
   # experiments = suite.get_exps("./results")
   # pprint.pprint(experiments)
 
-  analyzeResults("./results", suite)
+  summarizeResults("./results", suite)
 
   for expName in [
-    # "./results/experiment1", "./results/experiment2",
-    # "./results/experiment3", "./results/experiment4",
-    # "./results/experimentTemp", "./results/experiment7",
-    # "./results/experiment8", "./results/experiment10",
-    # "./results/experiment11",
-    # "./results/experiment12",
-    # "./results/experiment14",
-    # "./results/experiment15",
-    # "./results/experiment17",
-    # "./results/experiment19",
-    # "./results/experiment20",
-    # "./results/experiment21",
-    # "./results/experiment23",
-    # "./results/experiment23Best",
-    # "./results/experiment23Best2",
-    # "./results/experiment24",
     "./results/standardOneLayer",
     "./results/experiment28",
-    # "./results/experiment29",
   ]:
     analyzeParameters(expName, suite)
 
@@ -136,25 +139,10 @@ if __name__ == '__main__':
   # Print details of the best ones so far
 
   expPath = "./results/standardOneLayer"
-  printExperimentSpecifics(expPath, suite)
+  lastNoiseCurve(expPath, suite)
+  learningCurve(expPath, suite)
 
   expPath = "./results/bestSparseNet/boost_strength1.0k50.0n500.0"
-  printExperimentSpecifics(expPath, suite)
+  lastNoiseCurve(expPath, suite)
+  learningCurve(expPath, suite)
 
-  # expPath = "./results/experiment1/learning_rate0.040boost_strength0.0k100.0momentum0.250"
-  # printExperimentSpecifics(expPath, suite)
-  #
-  # expPath = "./results/experiment7/weight_sparsity0.350learning_rate0.040boost_strength1.50k200.0momentum0.250"
-  # printExperimentSpecifics(expPath, suite)
-  #
-  # expPath = "./results/experiment10/weight_sparsity0.40learning_rate0.040n500.0boost_strength1.0k50.0momentum0.250"
-  # printExperimentSpecifics(expPath, suite)
-
-  # expPath = "./results/experiment23/k_inference_factor2.0boost_strength_factor0.90learning_rate0.040batch_size4.0n500.0boost_strength1.0k50.0"
-  # printExperimentSpecifics(expPath, suite)
-  #
-  # expPath = "./results/experiment23Best/k_inference_factor1.50boost_strength_factor0.90learning_rate0.040batch_size4.0n500.0boost_strength1.0k50.0"
-  # printExperimentSpecifics(expPath, suite)
-  #
-  # expPath = "./results/experiment24/k_inference_factor1.50boost_strength_factor0.850learning_rate0.040batch_size4.0n1000.0boost_strength1.50k50.0"
-  # printExperimentSpecifics(expPath, suite)
