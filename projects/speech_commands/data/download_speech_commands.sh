eda594cf7016f3ca64d5521b920dcbdffd822730
Subutai Ahmad <sahmad@numenta.com>
2019-01-15 15:38:37 -0800

    oops

--- projects/speech_commands/data/download_speech_commands.sh
+++ projects/speech_commands/data/download_speech_commands.sh
@@ -4,14 +4,12 @@ set -e
 FILE_NAME=speech_commands_v0.01.tar.gz
 URL=http://download.tensorflow.org/data/$FILE_NAME
 echo "downloading $URL...\n"
-#wget $URL
+wget $URL
 
 DATASET_FOLDER=speech_commands_test
 echo "extracting $FILE_NAME..."
 mkdir -p $DATASET_FOLDER
 tar -xzf $FILE_NAME -C $DATASET_FOLDER
-
-#mv speech_commands_v0.01 $DATASET_FOLDER
 
 echo "splitting the dataset into train, validation and test sets..."
 python split_dataset.py --root=$DATASET_FOLDER --size=small
