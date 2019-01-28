4178087e2db94b64b791c9be19bb252dde68c4eb
Marcus Lewis <mrcslws@gmail.com>
2019-01-27 18:57:23 -0800

    Fix typo

--- projects/kdimgrid/runExperiments_oblique_normalized.sh
+++ projects/kdimgrid/runExperiments_oblique_normalized.sh
@@ -8,5 +8,5 @@ pwd
 DATA_DIR=data/oblique_normalized/$(python -c "import time; import uuid; print '{}-{}'.format(time.strftime('%Y%m%d-%H%M%S'), uuid.uuid1())")
 mkdir -p ./$DATA_DIR
 
-python -u generate_data.py $DATA_DIR --phaseResolution 0.2 --m 1 2 3 4 5 6 6 7 8 9 --k 3 4 5 6 --numTrials 100 --measureRectangle --allowOblique
+python -u generate_data.py $DATA_DIR --phaseResolution 0.2 --m 1 2 3 4 5 6 7 8 9 --k 3 4 5 6 --numTrials 100 --measureRectangle --allowOblique
 python -u measure_unique_sidelength.py $DATA_DIR --normalizeBasis
