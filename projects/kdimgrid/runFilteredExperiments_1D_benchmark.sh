cf0241a6ec75904f1de481ffc349dd3674e33025
Marcus Lewis <mrcslws@gmail.com>
2019-01-30 15:42:03 -0800

    Include multiple phase resolutions in benchmark data

--- projects/kdimgrid/runFilteredExperiments_1D_benchmark.sh
+++ projects/kdimgrid/runFilteredExperiments_1D_benchmark.sh
@@ -8,5 +8,5 @@ pwd
 DATA_DIR=data/1D_benchmark_filtered/$(python -c "import time; import uuid; print '{}-{}'.format(time.strftime('%Y%m%d-%H%M%S'), uuid.uuid1())")
 mkdir -p ./data/$DATA_DIR
 
-python -u generate_filtered_data.py $DATA_DIR --phaseResolution 0.2 --m 1 2 3 --k 1  --numTrials 2000
+python -u generate_filtered_data.py $DATA_DIR --phaseResolution 0.4 0.2 0.1 0.05 0.025 --m 1 2 3 --k 1  --numTrials 2000
 python -u measure_unique_sidelength_filtered.py $DATA_DIR
