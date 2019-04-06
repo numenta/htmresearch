c8b56a049156e33b526ed0d1a4303ea62b45d380
Subutai Ahmad <sahmad@Subutaiptop2019.attlocal.net>
2019-04-05 18:27:40 -0700

    Initial experiment version for CIFAR

---
+++ htmresearch/frameworks/pytorch/cifar_experiment.py
@@ -0,0 +1,238 @@
+# ----------------------------------------------------------------------
+# Numenta Platform for Intelligent Computing (NuPIC)
+# Copyright (C) 2019, Numenta, Inc.  Unless you have an agreement
+# with Numenta, Inc., for a separate license for this software code, the
+# following terms and conditions apply:
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero Public License for more details.
+#
+# You should have received a copy of the GNU Affero Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# http://numenta.org/licenses/
+# ----------------------------------------------------------------------
+from __future__ import print_function, division
+
+import os
+import random
+import time
+
+import pprint
+import numpy as np
+import torch
+import torch.nn as nn
+import tqdm
+from torch.utils.data import ConcatDataset
+from torchvision import transforms, datasets
+
+from htmresearch.frameworks.pytorch.model_utils import trainModel, evaluateModel
+from htmresearch.frameworks.pytorch.modules.not_so_densenet import (
+  densenet_cifar, notso_densenet_cifar
+)
+from htmresearch.support.expsuite import PyExperimentSuite
+
+
+
+class CIFARExperiment(PyExperimentSuite):
+  """
+  Baseline continual learning experiment based on incremental class learning
+  scenario using mnist dataset where we train on two MNIST categories at the
+  time while testing of all previous categories:
+
+      - train on [0, 1], test on [0, 1]
+      - train on [2, 3], test on [0, 1, 2, 3]
+      - ...
+      - train on [8, 9], test on [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  """
+
+
+  def __init__(self):
+    super(CIFARExperiment, self).__init__()
+
+    self.transform_train = transforms.Compose([
+      transforms.RandomCrop(32, padding=4),
+      transforms.RandomHorizontalFlip(),
+      transforms.ToTensor(),
+      transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    self.transform_test = transforms.Compose([
+      transforms.ToTensor(),
+      transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+
+    self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+  def do_experiment(self, params):
+    self.restore_supported = ("restore_supported" in self.cfgparser.defaults() and
+                              self.cfgparser.getboolean("DEFAULT", "restore_supported"))
+    super(CIFARExperiment, self).do_experiment(params)
+
+
+  def reset(self, params, repetition):
+
+    pprint.pprint(params)
+
+    self.initialize(params, repetition)
+
+    # Load CIFAR dataset
+    dataDir = params.get('dataDir', 'data')
+    self.trainset = datasets.CIFAR10(root=dataDir, train=True, download=True,
+                                transform=self.transform_train)
+    self.testset = datasets.CIFAR10(root=dataDir, train=False, download=True,
+                               transform=self.transform_test)
+
+    # Assume weight_sparsity == 1.0 for dense networks
+    if self.dense:
+      self.model = densenet_cifar(growth_rate=self.growth_rate)
+    else:
+      self.model = None
+
+    print("Setting device")
+    self.model.to(self.device)
+    if torch.cuda.device_count() > 1:
+      print("Using", torch.cuda.device_count(), "GPUs")
+      self.model = torch.nn.DataParallel(self.model)
+
+    self.optimizer = self.createOptimizer(self.model)
+    self.lr_scheduler = self.createLearningRateScheduler(self.optimizer)
+
+
+  def initialize(self, params, repetition):
+    """
+    Initialize experiment parameters and default values from configuration file.
+    Called at the beginning of each experiment and each repetition.
+    """
+    self.name = params["name"]
+    self.dataDir = params.get("datadir", "data")
+    self.seed = params.get("seed", 42) + repetition
+
+    torch.manual_seed(self.seed)
+    np.random.seed(self.seed)
+    random.seed(self.seed)
+
+    # Training
+    self.epochs = params.get("epochs", 1)
+    self.batch_size = params.get("batch_size", 128)
+    self.batches_in_epoch = params.get("batches_in_epoch", 100000)
+    self.first_epoch_batch_size = params.get("first_epoch_batch_size", self.batch_size)
+    self.batches_in_first_epoch = params.get("batches_in_first_epoch", self.batches_in_epoch)
+
+    # Testing
+    self.test_batch_size = params.get("test_batch_size", 1000)
+    self.test_batches_in_epoch = params.get("test_batches_in_epoch", 100000)
+
+    # Optimizer
+    self.optimizer_class = eval(params.get("optimizer", "torch.optim.SGD"))
+    self.lr = params.get("learning_rate", 0.1)
+    self.momentum = params.get("momentum", 0.9)
+    self.weight_decay = params.get("weight_decay", 0.0)
+    self.optimizer_params = eval(params.get("optimizer_params", "{}"))
+    self.lr_scheduler_gamma = params.get("lr_scheduler_gamma", 0.95)
+    self.loss_function = eval(params.get("loss_function", "torch.nn.functional.nll_loss"))
+
+    # Network parameters
+    self.dense = params.get("dense", True)
+    self.growth_rate = params.get("growth_rate", 12)
+
+
+
+  def createLearningRateScheduler(self, optimizer):
+    """
+    Creates the learning rate scheduler and attach the optimizer
+    """
+    return torch.optim.lr_scheduler.StepLR(optimizer,
+                                           step_size=1,
+                                           gamma=self.lr_scheduler_gamma)
+
+
+  def createOptimizer(self, model):
+    """
+    Create a new instance of the optimizer
+    """
+    return torch.optim.SGD(model.parameters(),
+                           lr=self.lr,
+                           momentum=self.momentum,
+                           weight_decay=self.weight_decay)
+
+
+  def iterate(self, params, repetition, iteration):
+
+    print("\nEpoch: {:d}".format(iteration))
+    t1 = time.time()
+
+    if iteration == 0:
+      batch_size = self.first_epoch_batch_size
+      batches_in_epoch = self.batches_in_first_epoch
+    else:
+      batch_size = self.batch_size
+      batches_in_epoch = self.batches_in_epoch
+
+    # Train on a single task
+    train_loader = torch.utils.data.DataLoader(dataset=self.trainset,
+                                               batch_size=batch_size,
+                                               shuffle=True)
+    self.preEpoch()
+    print("Learning rate: {:f}".format(self.lr_scheduler.get_lr()[0]))
+
+
+    trainModel(model=self.model, loader=train_loader,
+               optimizer=self.optimizer, device=self.device,
+               batches_in_epoch=batches_in_epoch,
+               criterion=self.loss_function)
+    self.postEpoch()
+
+    print("Training time for epoch=", time.time() - t1)
+
+    # Test on all trained tasks combined
+    test_loader = torch.utils.data.DataLoader(dataset=self.testset ,
+                                              batch_size=self.test_batch_size,
+                                              shuffle=True)
+    ret = evaluateModel(model=self.model, device=self.device,
+                         loader=test_loader,
+                         batches_in_epoch=self.test_batches_in_epoch,
+                         criterion=self.loss_function)
+
+    print("Loss = {:5.4f}, Accuracy = {:5.3f}%".format(ret["loss"], 100.0*ret["accuracy"]))
+    print("Full epoch time =", time.time() - t1)
+
+    return ret
+
+  def preEpoch(self):
+    if self.lr_scheduler is not None:
+      self.lr_scheduler.step()
+
+
+  def postEpoch(self):
+    self.model.postEpoch()
+
+
+  def save_state(self, params, rep, n):
+    saveDir = os.path.join(params["path"], params["name"], "model.{}.{}.pt".format(rep, n))
+    torch.save(self.model, saveDir)
+
+
+  def restore_state(self, params, rep, n):
+    saveDir = os.path.join(params["path"], params["name"], "model.{}.{}.pt".format(rep, n))
+    self.model = torch.load(saveDir, map_location=self.device)
+
+
+  def finalize(self, params, rep):
+    if params.get("restore_supported", False):
+      saveDir = os.path.join(params["path"], params["name"], "model.final.pt")
+      torch.save(self.model, saveDir)
+
+
+
+if __name__ == '__main__':
+  suite = CIFARExperiment()
+  suite.start()
---
+++ htmresearch/frameworks/pytorch/modules/not_so_densenet.py
@@ -0,0 +1,316 @@
+# ----------------------------------------------------------------------
+# Numenta Platform for Intelligent Computing (NuPIC)
+# Copyright (C) 2019, Numenta, Inc.  Unless you have an agreement
+# with Numenta, Inc., for a separate license for this software code, the
+# following terms and conditions apply:
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero Public License for more details.
+#
+# You should have received a copy of the GNU Affero Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# http://numenta.org/licenses/
+# ----------------------------------------------------------------------
+import math
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import numpy as np
+
+from tabulate import tabulate
+
+from htmresearch.frameworks.pytorch.modules import (
+  SparseWeights, KWinners2d, KWinners,
+  updateBoostStrength, rezeroWeights)
+
+
+
+
+class SparseBottleneck(nn.Module):
+  def __init__(self, in_planes, growth_rate, input_width, sparsity=0.1):
+    super(SparseBottleneck, self).__init__()
+    self.bn1 = nn.BatchNorm2d(in_planes)
+    self.conv1 = nn.Conv2d(in_planes, 4*growth_rate, kernel_size=1, bias=False)
+    self.bn2 = nn.BatchNorm2d(4*growth_rate)
+    self.conv2 = nn.Conv2d(4*growth_rate, growth_rate, kernel_size=3, padding=1, bias=False)
+
+    conv2OutputSize = (growth_rate * input_width * input_width)
+    self.k = int(sparsity * conv2OutputSize)
+    if sparsity < 0.5:
+      self.kwinners2 = KWinners2d(
+        n=conv2OutputSize, k=self.k, channels=growth_rate,
+        kInferenceFactor=1.25, boostStrength=1.5, boostStrengthFactor=0.95)
+      print "SparseBottleneck init: in_planes:", in_planes, "conv2OutputSize:", conv2OutputSize, "k:", self.k
+    else:
+      print "SparseBottleneck init: Skipping k-winners, sparsity too high"
+      self.kwinners2 = None
+
+  def forward(self, x):
+    out = self.conv1(F.relu(self.bn1(x)))
+    out = self.conv2(F.relu(self.bn2(out)))
+    if self.kwinners2 is not None:
+      out = self.kwinners2(out)
+    out = torch.cat([out,x], 1)
+    return out
+
+
+
+
+class SparseTransition(nn.Module):
+  def __init__(self, in_planes, out_planes, imSize=32*32, sparsity=0.1):
+    super(SparseTransition, self).__init__()
+    self.bn = nn.BatchNorm2d(in_planes)
+    self.conv = nn.Conv2d(in_planes, out_planes, kernel_size=1, bias=False)
+
+    transitionOutputSize = in_planes * imSize / 4
+    self.k = int(sparsity * transitionOutputSize)
+    if sparsity < 0.5:
+      self.kwinners = KWinners2d(
+        n=transitionOutputSize, k=self.k, channels=out_planes,
+        kInferenceFactor=1.25,
+        boostStrength=1.5,
+        boostStrengthFactor=0.95)
+      print "Sparse Transition init: in_planes:", in_planes, "out_planes:", out_planes, "k:", self.k
+    else:
+      print "Sparse Transition init: Skipping k-winners, sparsity too high"
+      self.kwinners = None
+
+
+  def forward(self, x):
+    out = self.bn(x)
+    out = self.conv(out)
+    out = F.avg_pool2d(out, 2)
+    if self.kwinners is not None:
+      out = self.kwinners(out)
+    return out
+
+
+class NotSoDenseNet(nn.Module):
+  def __init__(self, block, nblocks, growth_rate=12, reduction=0.5, num_classes=10,
+               dense_sparsities=[0.1, 0.1, 0.2, 0.5],
+               transition_sparsities=[0.1, 0.1, 0.1],
+               linear_sparsity=0.1,
+               linear_weight_sparsity=0.3,
+               image_width=32):
+    super(NotSoDenseNet, self).__init__()
+    self.growth_rate = growth_rate
+    self.iteration = 0
+    self.linear_sparsity = linear_sparsity
+
+    print "Creating Sparse network with nblocks=",nblocks,"and growth_rate=",growth_rate
+    print "dense_sparsities=", dense_sparsities
+    print "transition_sparsities=",transition_sparsities
+    print "linear_sparsity=", linear_sparsity, "linear_weight_sparsity=", linear_weight_sparsity
+
+    num_planes = 2*growth_rate
+    print
+    print "Creating first CNN layer with out_channels=",num_planes
+    self.conv1 = nn.Conv2d(3, num_planes, kernel_size=3, padding=1, bias=False)
+
+    print
+    print "Creating dense block 1 with num blocks=",nblocks[0]
+    self.dense1 = self._make_dense_layers(block, num_planes,
+                                          nblocks[0], image_width,
+                                          sparsity=dense_sparsities[0])
+    num_planes += nblocks[0]*growth_rate
+    out_planes = int(math.floor(num_planes*reduction))
+    # self.trans1 = Transition(num_planes, out_planes)
+    print "Transition. in_channels:", num_planes, "| out_channels:", out_planes
+    self.trans1 = SparseTransition(num_planes, out_planes,
+                                   imSize=image_width*image_width,
+                                   sparsity=transition_sparsities[0])
+    num_planes = out_planes
+
+    print
+    print "Creating dense block 2 with num blocks=",nblocks[1]
+    self.dense2 = self._make_dense_layers(block, num_planes,
+                                          nblocks[1], image_width / 2,
+                                          sparsity=dense_sparsities[1])
+    num_planes += nblocks[1]*growth_rate
+    out_planes = int(math.floor(num_planes*reduction))
+    # self.trans2 = Transition(num_planes, out_planes)
+    print "Transition. in_channels:", num_planes, "| out_channels:", out_planes
+    self.trans2 = SparseTransition(num_planes, out_planes,
+                                   imSize=16 * 16,
+                                   sparsity=transition_sparsities[1])
+    num_planes = out_planes
+
+    print
+    print "Creating dense block 3 with num blocks=",nblocks[2]
+    self.dense3 = self._make_dense_layers(block, num_planes,
+                                          nblocks[2], image_width / 4,
+                                          sparsity=dense_sparsities[2])
+    num_planes += nblocks[2]*growth_rate
+    out_planes = int(math.floor(num_planes*reduction))
+    # self.trans3 = Transition(num_planes, out_planes)
+    print "Transition. in_channels:", num_planes, "| out_channels:", out_planes
+    self.trans3 = SparseTransition(num_planes, out_planes,
+                                   imSize=8 * 8,
+                                   sparsity=transition_sparsities[2])
+    num_planes = out_planes
+
+    print
+    print "Creating dense block 4 with num blocks=",nblocks[3]
+    self.dense4 = self._make_dense_layers(block, num_planes,
+                                          nblocks[3], image_width / 8,
+                                          sparsity=dense_sparsities[3])
+    num_planes += nblocks[3]*growth_rate
+
+    self.bn = nn.BatchNorm2d(num_planes)
+
+    if self.linear_sparsity > 0:
+      print "Number of inputs into linearSDR=", num_planes
+      print "linearSDR weightSparsity = 0.3, k=50/500"
+      self.linear1 = SparseWeights(nn.Linear(num_planes, 500),
+                                   weightSparsity=linear_weight_sparsity)
+      k = int(500*linear_sparsity)
+      self.linear1KWinners = KWinners(
+        n=500, k=k, kInferenceFactor=1.5,
+        boostStrength=1.5,
+        boostStrengthFactor=0.95)
+      self.linearOut = nn.Linear(500, num_classes)
+    else:
+      print "Skipping linear sparse layer"
+      self.linear1KWinners = None
+      self.linearOut = nn.Linear(num_planes, num_classes)
+
+
+  def _make_dense_layers(self, block, in_planes, nblock, input_width, sparsity):
+    layers = []
+    for i in range(nblock):
+      layers.append(block(in_planes, self.growth_rate, input_width, sparsity=sparsity))
+      in_planes += self.growth_rate
+
+    for l,layer in enumerate(layers):
+      print "Layer:",l,"in_channels for conv1:",layer.conv1.in_channels,
+      print "| in_channels for conv2:", layer.conv2.in_channels, "| out_channels for conv2:", layer.conv2.out_channels
+
+    return nn.Sequential(*layers)
+
+
+  def forwardWithTable(self, x):
+    paramsTable = [["Name", "out_channels", "Output Size", "k", "Non-zeros"]]
+
+    paramsTable.append(["Image", x.shape[1], x.shape[1]*x.shape[2]*x.shape[3],
+                        0, x[0].nonzero().size(0)])
+
+    out = self.conv1(x)
+    paramsTable.append(["Conv1", out.shape[1], out.shape[1]*out.shape[2]*out.shape[3],
+                        0, out[0].nonzero().size(0)])
+
+    out = self.dense1(out)
+    paramsTable.append(["Dense1", out.shape[1], out.shape[1]*out.shape[2]*out.shape[3],
+                        0, out[0].nonzero().size(0)])
+
+    out = self.trans1(out)
+    paramsTable.append(["Trans1", out.shape[1], out.shape[1]*out.shape[2]*out.shape[3],
+                        0, out[0].nonzero().size(0)])
+
+    out = self.dense2(out)
+    paramsTable.append(["Dense2", out.shape[1], out.shape[1]*out.shape[2]*out.shape[3],
+                        0, out[0].nonzero().size(0)])
+
+    out = self.trans2(out)
+    paramsTable.append(["Trans2", out.shape[1], out.shape[1]*out.shape[2]*out.shape[3],
+                        0, out[0].nonzero().size(0)])
+
+    out = self.dense3(out)
+    paramsTable.append(["Dense3", out.shape[1], out.shape[1]*out.shape[2]*out.shape[3],
+                        0, out[0].nonzero().size(0)])
+
+    out = self.trans3(out)
+    paramsTable.append(["Trans3", out.shape[1], out.shape[1]*out.shape[2]*out.shape[3],
+                        0, out[0].nonzero().size(0)])
+
+    out = self.dense4(out)
+    paramsTable.append(["Dense4", out.shape[1], out.shape[1]*out.shape[2]*out.shape[3],
+                        0, out[0].nonzero().size(0)])
+
+    out = F.avg_pool2d(F.relu(self.bn(out)), 4)
+    paramsTable.append(["AvgPool", out.shape[1], out.shape[1]*out.shape[2]*out.shape[3],
+                        0, out[0].nonzero().size(0)])
+
+    out = out.view(out.size(0), -1)
+
+    if self.linear_sparsity > 0:
+      out = self.linear1KWinners(self.linear1(out))
+      paramsTable.append(["LinearSDR", 1, out.shape[1],
+                          0, out[0].nonzero().size(0)])
+
+    out = self.linearOut(out)
+
+    print ""
+    print tabulate(paramsTable, headers="firstrow", tablefmt="grid")
+
+    return out
+
+
+  def forward(self, x):
+    self.iteration += 1
+    if self.iteration == 1:
+      return self.forwardWithTable(x)
+
+    out = self.conv1(x)
+    out = self.trans1(self.dense1(out))
+    out = self.trans2(self.dense2(out))
+    out = self.trans3(self.dense3(out))
+    out = self.dense4(out)
+    out = F.avg_pool2d(F.relu(self.bn(out)), 4)
+    out = out.view(out.size(0), -1)
+    if self.linear_sparsity > 0:
+      out = self.linear1KWinners(self.linear1(out))
+
+    out = self.linearOut(out)
+
+    return out
+
+  def postEpoch(self):
+    self.apply(updateBoostStrength)
+    self.apply(rezeroWeights)
+    if self.linear1KWinners is not None:
+      print "boostStrength is now:", self.linear1KWinners.boostStrength
+
+
+# def DenseNet121():
+#   return DenseNet(Bottleneck, [6,12,24,16], growth_rate=32)
+#
+# def SparseDenseNet121():
+#   return NotSoDenseNet(Bottleneck, [6, 12, 24, 16], growth_rate=32)
+#
+# def DenseNet169():
+#   return DenseNet(Bottleneck, [6,12,32,32], growth_rate=32)
+#
+# def DenseNet201():
+#   return DenseNet(Bottleneck, [6,12,48,32], growth_rate=32)
+#
+# def DenseNet161():
+#   return DenseNet(Bottleneck, [6,12,36,24], growth_rate=48)
+
+def densenet_cifar(growth_rate=12):
+  print "Running densenet_cifar with growth rate", growth_rate
+  return NotSoDenseNet(SparseBottleneck, [6, 12, 24, 16],
+                       growth_rate=growth_rate,
+                       dense_sparsities=[1.0]*4,
+                       transition_sparsities=[1.0]*3,
+                       linear_sparsity=0.0
+                       )
+
+def notso_densenet_cifar(growth_rate=12):
+  print "Running notso_densenet_cifar with growth rate=", growth_rate
+  return NotSoDenseNet(SparseBottleneck, [6, 12, 24, 16],
+                       growth_rate=growth_rate,
+                       dense_sparsities=[0.2, 1.0, 1.0, 1.0],
+                       transition_sparsities=[0.1, 0.1, 0.2],
+                       linear_sparsity=0.0,
+                       linear_weight_sparsity=0.3,
+                       )
+
